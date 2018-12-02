966b944a4a2290f668de6d587d1c2ddffc7faed3
sueka <mail@sueka.me>
2018-12-02 19:41:39 +0900

    :sparkles: Create Gulp tasks

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,10 +1,13 @@
 import { TaskFunction, parallel, series } from 'gulp'
+import * as del from 'del'
 import { exec } from 'child_process'
 
+export const clean: TaskFunction = () => del(['dist', '**/*.css.d.ts', '!node_modules/**'])
 export const typeCheck = series(sh('tcm src -s'), sh('tsc --noEmit -p .'))
 const tslint = sh('tslint -p .')
 const stylelint = sh('stylelint src')
 export const staticCheck = parallel(typeCheck, tslint, stylelint)
+export const build = series(staticCheck, sh('parcel build src/index.html'))
 
 function sh(cmd: string) {
   const task: TaskFunction = () => {
