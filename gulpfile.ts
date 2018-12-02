06e0bdf0dd45d5814dac06bf085d35062f845245
sueka <mail@sueka.me>
2018-12-02 21:27:17 +0900

    :art: Rename helper

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,13 +3,13 @@ import * as del from 'del'
 import { exec } from 'child_process'
 
 export const clean: TaskFunction = () => del(['dist', '**/*.css.d.ts', '!node_modules/**'])
-export const typeCheck = series(sh('tcm src -s'), sh('tsc --noEmit -p .'))
-const tslint = sh('tslint -p .')
-const stylelint = sh('stylelint src')
+export const typeCheck = series(npx('tcm src -s'), npx('tsc --noEmit -p .'))
+const tslint = npx('tslint -p .')
+const stylelint = npx('stylelint src')
 export const staticCheck = parallel(typeCheck, tslint, stylelint)
-export const build = series(staticCheck, sh('parcel build src/index.html'))
+export const build = series(staticCheck, npx('parcel build src/index.html'))
 
-function sh(cmd: string) {
+function npx(cmd: string) {
   const task: TaskFunction = () => {
     const cp = exec(cmd)
 
