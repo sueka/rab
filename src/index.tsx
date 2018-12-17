eb3ab4e8dcdc41644928c80e1ab7821c984d7a74
sueka <mail@sueka.me>
2018-12-17 22:19:44 +0900

    :art: Format

--- src/index.tsx
+++ src/index.tsx
@@ -18,8 +18,8 @@ const { store, sagaMiddleware } = configureStore(history)
 sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
+  <Provider {...{ store }}>
+    <ConnectedRouter {...{ history }}>
       <App />
     </ConnectedRouter>
   </Provider>,
