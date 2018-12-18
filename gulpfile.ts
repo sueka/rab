4b4b365f82946eb80bf41e9c544e0fd294067312
sueka <mail@sueka.me>
2018-12-18 22:14:10 +0900

    :wrench: Not track JavaScript code

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,7 +2,7 @@ import { TaskFunction, parallel, series } from 'gulp'
 import * as del from 'del'
 import { exec } from 'child_process'
 
-export const clean: TaskFunction = () => del(['dist', '**/*.css.d.ts', '!node_modules/**'])
+export const clean: TaskFunction = () => del(['dist', '**/*.css.d.ts', '**/*.js{,x}', '!node_modules/**'])
 export const typeCheck = series(npx('tcm src -s'), npx('tsc --noEmit -p .'))
 const tslint = npx('tslint -p .')
 const stylelint = npx('stylelint src')
