4bba51fdfe49c884d2bf9e6df44816671f5c425c
sueka <mail@sueka.me>
2018-12-27 16:00:59 +0900

    :sparkles: Use CSS Modules on Storybook

--- .storybook/webpack.config.ts
+++ .storybook/webpack.config.ts
@@ -1,13 +1,25 @@
 import { Configuration } from 'webpack'
 
-// NOTE: ! を使い、 defaultConfig が所望の構造でなければ実行時 TypeError が発生するようにしている。
-module.exports = (_baseConfig: unknown, _env: unknown, defaultConfig: Configuration) => {
-  defaultConfig.module!.rules.push({
+// NOTE: ! を使い、 baseConfig が所望の構造でなければ実行時 TypeError が発生するようにしている。
+module.exports = (baseConfig: Configuration) => {
+  baseConfig.module!.rules.push({
+    test: /\.css$/,
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          modules: true,
+        }
+      },
+      'postcss-loader',
+    ],
+  }, {
     test: /\.tsx?$/,
     loader: 'awesome-typescript-loader',
   })
 
-  defaultConfig.resolve!.extensions!.push('.ts', '.tsx')
+  baseConfig.resolve!.extensions!.push('.ts', '.tsx')
 
-  return defaultConfig
+  return baseConfig
 }
