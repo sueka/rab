2e86f83f9264c7732b9da6362a1b79f292334929
sueka <mail@sueka.me>
2018-12-29 21:59:20 +0900

    :hammer: Use local state of components instead of instance property

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -13,21 +13,36 @@ export interface DispatchProps {
 
 type Props = StateProps & DispatchProps
 
-export default class Info extends React.Component<Props> {
-  private callId: string | null = null
+interface LocalState {
+  callId?: string
+}
+
+export default class Info extends React.Component<Props, LocalState> {
+
+  // NOTE: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/826ce0f1ce1d1887d199986283630d6f63075ad5/types/react/index.d.ts#L419 にも関わらず、初期化されていない state は null であるため、初期化を強制するためにプロパティ宣言を行う。
+  public state: Readonly<LocalState>
+
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {}
+  }
 
   public componentDidMount() {
     const { _tryToFetch } = this.props
 
     const { payload } = _tryToFetch('GET', 'https://api.github.com/repos/sueka/react-app-prototype')
 
-    this.callId = payload.callId
+    this.setState({
+      callId: payload.callId,
+    })
   }
 
   public render() {
     const { calls } = this.props
+    const { callId } = this.state
 
-    const call = calls.find(({ id }) => (this.callId !== null) ? this.callId === id : false)
+    const call = calls.find(({ id }) => (callId !== undefined) ? callId === id : false)
 
     if (call === undefined) {
       return (
