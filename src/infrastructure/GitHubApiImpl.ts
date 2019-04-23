a14766c439e7a01d03c615bc245b00f61898df80
sueka <mail@sueka.me>
2019-04-23 23:51:56 +0900

    :bug: Fix type errors

--- src/infrastructure/GitHubApiImpl.ts
+++ src/infrastructure/GitHubApiImpl.ts
@@ -1,10 +1,7 @@
-import { injectable } from 'inversify'
-
 import { Repository } from '../githubResourceTypes'
 import { HttpClient } from '../lib'
 import GitHubApi, { GetRepoInput, GetRepoOutput } from '../useCase/GitHubApi'
 
-@injectable()
 export default class GitHubApiImpl implements GitHubApi {
   private httpClient: HttpClient
 
