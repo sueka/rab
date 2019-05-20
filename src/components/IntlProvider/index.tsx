329eab1c77c8234021d7f9653e5b486071d0cca8
sueka <mail@sueka.me>
2019-05-21 07:39:03 +0900

    :memo: Add a TODO comment

--- src/components/IntlProvider/index.tsx
+++ src/components/IntlProvider/index.tsx
@@ -6,6 +6,7 @@ export type StateProps = Pick<IntlProvider.Props, 'locale' | 'messages'>
 type Props =
   & StateProps
 
+// TODO: intl context でない要素を re-render しないようにする。 https://github.com/formatjs/react-intl/issues/234#issuecomment-163366518 によると現時点では難しいらしい。
 // cf. https://github.com/formatjs/react-intl/issues/371#issuecomment-275703796
 const IntlProviderWrapper: React.FunctionComponent<Props> = (props) => (
   <IntlProvider key={ props.locale } { ...props } />
