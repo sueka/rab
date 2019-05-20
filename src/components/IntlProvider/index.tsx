72a104243066ccac295284a1841702cc12b32871
sueka <mail@sueka.me>
2019-05-21 04:22:37 +0900

    :sparkles: Update locale instantly

--- src/components/IntlProvider/index.tsx
+++ src/components/IntlProvider/index.tsx
@@ -1,5 +1,14 @@
+import * as React from 'react'
 import { IntlProvider } from 'react-intl'
 
 export type StateProps = Pick<IntlProvider.Props, 'locale' | 'messages'>
 
-export default IntlProvider
+type Props =
+  & StateProps
+
+// cf. https://github.com/formatjs/react-intl/issues/371#issuecomment-275703796
+const IntlProviderWrapper: React.FunctionComponent<Props> = (props) => (
+  <IntlProvider key={ props.locale } { ...props } />
+)
+
+export default IntlProviderWrapper
