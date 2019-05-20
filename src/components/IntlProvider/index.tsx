7198fe2cb905c0ef3a7048e72e1754243fc3b7c0
sueka <mail@sueka.me>
2019-05-21 08:35:25 +0900

    :hammer: Rename identifiers

--- src/components/IntlProvider/index.tsx
+++ src/components/IntlProvider/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
-import { IntlProvider } from 'react-intl'
+import { IntlProvider as OriginalIntlProvider } from 'react-intl'
 
-export type StateProps = Pick<IntlProvider.Props, 'locale' | 'messages'>
+export type StateProps = Pick<OriginalIntlProvider.Props, 'locale' | 'messages'>
 
 type Props =
   & StateProps
 
 // TODO: intl context でない要素を re-render しないようにする。 https://github.com/formatjs/react-intl/issues/234#issuecomment-163366518 によると現時点では難しいらしい。
 // cf. https://github.com/formatjs/react-intl/issues/371#issuecomment-275703796
-const IntlProviderWrapper: React.FunctionComponent<Props> = (props) => (
-  <IntlProvider key={ props.locale } { ...props } />
+const IntlProvider: React.FunctionComponent<Props> = (props) => (
+  <OriginalIntlProvider key={ props.locale } { ...props } />
 )
 
-export default IntlProviderWrapper
+export default IntlProvider
