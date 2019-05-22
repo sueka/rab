3071bd999a89aaf325e53b60c32aa2bacae55d35
sueka <mail@sueka.me>
2019-05-22 16:17:58 +0900

    :lipstick: Preserve terminal color

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,6 @@
 import { TaskFunction, Globs, parallel, series, watch } from 'gulp'
 import del from 'del'
-import { exec } from 'child_process'
+import { spawn } from 'child_process'
 
 const ignored = ['.cache', 'coverage', 'dist', 'storybook-static', '**/*.css.d.ts', '**/*.js{,x}', '!jest.config.js']
 
@@ -14,14 +14,14 @@ const ignored = ['.cache', 'coverage', 'dist', 'storybook-static', '**/*.css.d.t
 //
 
 export const clean: TaskFunction = () => del([...ignored, '!node_modules/**', '!.env'])
-const extractMessages = npxTask('extract-messages --flat --default-locale=en --locales=en,ja --output=\'public/locales\' \'src/**/messages.ts\'')
-const preTypeCheck = parallel(npxTask('tcm src -s'), extractMessages)
-export const typeCheck = series(preTypeCheck, npxTask('tsc --noEmit -p .'))
-const tslint = npxTask('tslint -p .')
-const stylelint = npxTask('stylelint src')
+const extractMessages = npxTask('extract-messages', ['--flat', '--default-locale=en', '--locales=en,ja', '--output=public/locales', 'src/**/messages.ts'])
+const preTypeCheck = parallel(npxTask('tcm', ['src', '-s']), extractMessages)
+export const typeCheck = series(preTypeCheck, npxTask('tsc', ['--noEmit', '-p', '.']))
+const tslint = npxTask('tslint', ['-p', '.'])
+const stylelint = npxTask('stylelint', ['src'])
 export const staticCheck = namedTask('staticCheck', parallel(series(typeCheck, tslint), stylelint))
 const testWithoutCoverage = series(preTypeCheck, npxTask('jest'))
-const testWithCoverage = series(preTypeCheck, npxTask('jest --coverage'))
+const testWithCoverage = series(preTypeCheck, npxTask('jest', ['--coverage']))
 export const test = testWithCoverage
 export const build = series(typeCheck, npxTask('webpack'))
 export const buildStorybook = series(typeCheck, npxTask('build-storybook'))
@@ -29,9 +29,9 @@ export const buildStorybook = series(typeCheck, npxTask('build-storybook'))
 export const develop = parallel(
   continuousTask('src/**/messages.ts', extractMessages),
   continuousTask('src', staticCheck),
-  series(preTypeCheck, npxTask('jest --watch --watchPathIgnorePatterns \'\\.css\\.d\\.ts$\'')),
-  npxTask('webpack-dev-server --config webpack.config.dev.ts'),
-  npxTask('start-storybook --ci --quiet -p 5678'),
+  series(preTypeCheck, npxTask('jest', ['--watch', '--watchPathIgnorePatterns', '\'\\.css\\.d\\.ts$\''])),
+  npxTask('webpack-dev-server', ['--config', 'webpack.config.dev.ts']),
+  npxTask('start-storybook', ['--ci', '--quiet', '-p', '5678']),
 )
 
 export default series(testWithoutCoverage, build)
@@ -45,19 +45,14 @@ export default series(testWithoutCoverage, build)
 //                         _|
 //                         _|
 
-function npx(cmd: string) {
-  const cp = exec(cmd)
-
-  cp.stdout!.pipe(process.stdout)
-  cp.stderr!.pipe(process.stderr)
-
-  return cp
+function npx(util: string, args: string[]) {
+  return spawn(util, args, { stdio: 'inherit' })
 }
 
-function npxTask(cmd: string) {
-  const task: TaskFunction = () => npx(cmd)
+function npxTask(util: string, args: string[] = []) {
+  const task: TaskFunction = () => npx(util, args)
 
-  task.displayName = cmd
+  task.displayName = `${ util } ${ args.join(' ') }`
 
   return task
 }
