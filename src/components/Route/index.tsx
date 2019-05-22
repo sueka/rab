f63cad32d35013b2057baf93f8f1e107d612cfe8
sueka <mail@sueka.me>
2019-05-22 13:43:10 +0900

    :hammer: Suspend LazyExoticComponents only

--- src/components/Route/index.tsx
+++ src/components/Route/index.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react'
-import { Route as OriginalRoute, RouteProps as Props, RouteComponentProps } from 'react-router'
+import { Omit } from 'react-redux'
+import { Route as OriginalRoute, RouteProps, RouteComponentProps } from 'react-router'
 
 import ErrorBoundary from '../ErrorBoundary'
 
-// TODO: ban mandatory props
 const withErrorBoundary: (Component: React.ComponentType<RouteComponentProps> | React.ComponentType<unknown>) => React.FunctionComponent<RouteComponentProps> = (Component) => (props) => (
   <ErrorBoundary>
-    <React.Suspense fallback={ <div>loading..</div> }>
-      <Component { ...props } />
-    </React.Suspense>
+    <Component { ...props } />
   </ErrorBoundary>
 )
+
+const withSuspense: (Component: React.LazyExoticComponent<React.ComponentType<RouteComponentProps> | React.ComponentType<unknown>>) => React.FunctionComponent<RouteComponentProps> = (Component) => (props) => (
+  <React.Suspense fallback={ <div>loading..</div> }>
+    <Component { ...props } />
+  </React.Suspense>
+)
+
+interface Props extends Omit<RouteProps, 'render' | 'component' | 'children'> {
+  // TODO: ban mandatory props
+  component?: Required<RouteProps>['component'] | React.LazyExoticComponent<Required<RouteProps>['component']>
+}
 
 class Route extends React.Component<Props> {
   public render() {
@@ -20,7 +29,12 @@ class Route extends React.Component<Props> {
       return <OriginalRoute { ...restProps } />
     }
 
-    return <OriginalRoute component={ withErrorBoundary(component) } { ...restProps } />
+    if ('_result' in component) { // FIXME: if LazyExoticComponent
+      return <OriginalRoute component={ withErrorBoundary(withSuspense(component)) } { ...restProps } />
+    } else {
+      return <OriginalRoute component={ withErrorBoundary(component) } { ...restProps } />
+    }
+
   }
 }
 
