2d8fa7c3a93ba6c2d0821c153996d61fb9a4bb85
sueka <mail@sueka.me>
2019-05-25 18:30:27 +0900

    :rotating_light: Get more type-safe

--- src/config/ConfigRegistry.ts
+++ src/config/ConfigRegistry.ts
@@ -2,5 +2,5 @@ export type ConfigKey =
   | 'GITHUB_API_V3_ORIGIN'
 
 export default interface ConfigRegistry {
-  get(name: ConfigKey): unknown
+  get(name: ConfigKey): string
 }
--- src/infrastructure/EnvVarConfigRegistry.ts
+++ src/infrastructure/EnvVarConfigRegistry.ts
@@ -6,7 +6,7 @@ import ConfigRegistry, { ConfigKey } from '../config/ConfigRegistry'
 
 @injectable()
 export default class EnvVarConfigRegistry implements ConfigRegistry {
-  private env: Record<ConfigKey, unknown | undefined> = {
+  private env: Record<ConfigKey, string | undefined> = {
     GITHUB_API_V3_ORIGIN: process.env.GITHUB_API_V3_ORIGIN,
   }
 
