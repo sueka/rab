fdef485080bbc2639f0b824451ed0d1d5a57a09e
sueka <mail@sueka.me>
2019-07-10 03:09:14 +0900

    :wrench: Exclude externals, tests, react-intl messages & stories

--- typedoc.js
+++ typedoc.js
@@ -1,4 +1,10 @@
 module.exports = {
   out: 'doc',
   ignoreCompilerErrors: true,
+  excludeExternals: true,
+  exclude: [
+    '**/{*.,}{test,spec}.ts{x,}',
+    'src/components/**/messages.ts',
+    'src/{components,containers}/**/{*.,}stories.ts{x,}',
+  ],
 }
