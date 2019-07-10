12c7b34236b8b8b8a43e0126efb5edb8b618b8c4
sueka <mail@sueka.me>
2019-07-10 21:15:45 +0900

    :traffic_light: Not use {}

--- src/components/ErrorBoundary/index.ts
+++ src/components/ErrorBoundary/index.ts
@@ -5,7 +5,7 @@ interface LocalState {
   error?: unknown
 }
 
-class ErrorBoundary extends React.Component<{}, LocalState> {
+class ErrorBoundary extends React.Component<unknown, LocalState> {
 
   // NOTE: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/826ce0f1ce1d1887d199986283630d6f63075ad5/types/react/index.d.ts#L419 にも関わらず、初期化されていない state は null であるため、初期化を強制するためにプロパティ宣言を行う。
   public state: Readonly<LocalState> = {
