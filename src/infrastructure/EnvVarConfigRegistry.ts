e1337218998851c7ef6a85f610e07d762fc2cfab
sueka <mail@sueka.me>
2019-07-11 23:21:33 +0900

    :hammer: Nullablify results of ConfigRegistry.get

--- src/infrastructure/EnvVarConfigRegistry.ts
+++ src/infrastructure/EnvVarConfigRegistry.ts
@@ -1,7 +1,5 @@
-import * as assert from 'assert'
 import { injectable } from 'inversify'
 
-import { UnreachableError } from '../lib/errors'
 import ConfigRegistry, { ConfigKey } from '../config/ConfigRegistry'
 
 @injectable()
@@ -10,18 +8,7 @@ export default class EnvVarConfigRegistry implements ConfigRegistry {
     GITHUB_API_V3_ORIGIN: process.env.GITHUB_API_V3_ORIGIN,
   }
 
-  /**
-   * @throws {AssertionError|[[UnreachableError]]}
-   */
   public get(name: ConfigKey) {
-    const value = this.env[name]
-
-    assert(value !== undefined, `The ${ name } environment variable does not exist.`)
-
-    if (value === undefined) {
-      throw new UnreachableError() // for type guarding
-    }
-
-    return value
+    return this.env[name]
   }
 }
