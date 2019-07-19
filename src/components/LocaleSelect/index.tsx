a44f508afefe79e9714213f0e0e9f446f4fad555
sueka <mail@sueka.me>
2019-07-19 15:19:51 +0900

    :traffic_light: Add label for dropdown

--- src/components/LocaleSelect/index.tsx
+++ src/components/LocaleSelect/index.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react'
+import { FormattedMessage } from 'react-intl'
 
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
 import NativeSelect from '@material-ui/core/NativeSelect'
 
 import { Code, isCode, getNativeNameByCode } from '../../lib/languageNameSolver'
+
+import messages from './messages'
 
 export interface StateProps {
   availableLocales: Code[]
@@ -30,11 +35,16 @@ export default class LocaleSelect extends React.Component<Props> {
     const { availableLocales, locale } = this.props
 
     return (
-      <NativeSelect value={ locale } onChange={ this.handleChange }>
-        { availableLocales.map((availableLocale, i) => (
-          <option key={ i } value={ availableLocale }>{ getNativeNameByCode(availableLocale) }</option>
-        )) }
-      </NativeSelect>
+      <FormControl>
+        <InputLabel>
+          <FormattedMessage { ...messages.languages } />
+        </InputLabel>
+        <NativeSelect value={ locale } onChange={ this.handleChange }>
+          { availableLocales.map((availableLocale, i) => (
+            <option key={ i } value={ availableLocale }>{ getNativeNameByCode(availableLocale) }</option>
+          )) }
+        </NativeSelect>
+      </FormControl>
     )
   }
 }
---
+++ src/components/LocaleSelect/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  languages: {
+    id: 'src.components.LocaleSelect.languages',
+    defaultMessage: 'Languages',
+  },
+})
