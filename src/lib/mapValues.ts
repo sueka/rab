ff2daf432e42600028e23bead75d3e033f7a4e6a
sueka <mail@sueka.me>
2019-07-25 15:36:08 +0900

    :hammer: Rename a parameter

--- src/lib/mapValues.ts
+++ src/lib/mapValues.ts
@@ -1,8 +1,8 @@
-export default function mapValues<T, U>(f: (x: T) => U, input: Record<string, T>): Record<string, U> {
+export default function mapValues<T, U>(f: (x: T) => U, object: Record<string, T>): Record<string, U> {
   const result: Record<string, U> = {}
 
   // tslint:disable-next-line:no-loop-statement
-  for (const [i, x] of Object.entries(input)) {
+  for (const [i, x] of Object.entries(object)) {
     // tslint:disable-next-line:no-object-mutation
     result[i] = f(x)
   }
