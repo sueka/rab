37896d3c172724c13c6888154c108983538928ed
sueka <mail@sueka.me>
2019-07-28 11:23:19 +0900

    :hammer: Rename variables

--- src/lib/equalsJsons.ts
+++ src/lib/equalsJsons.ts
@@ -1,30 +1,30 @@
 import zipIterables from './zip/zipIterables'
 
-export default function equalsJsons(a: Json, b: Json): boolean {
-  if (a === null && b === null) {
+export default function equalsJsons(x: Json, y: Json): boolean {
+  if (x === null && y === null) {
     return true
   }
 
   if (
-    typeof a === 'boolean' && typeof b === 'boolean' ||
-    typeof a === 'number' && typeof b === 'number' ||
-    typeof a === 'string' && typeof b === 'string'
+    typeof x === 'boolean' && typeof y === 'boolean' ||
+    typeof x === 'number' && typeof y === 'number' ||
+    typeof x === 'string' && typeof y === 'string'
   ) {
-    return a === b
+    return x === y
   }
 
-  if (Array.isArray(a) && Array.isArray(b)) {
-    return equalsJsonArrays(a, b)
+  if (Array.isArray(x) && Array.isArray(y)) {
+    return equalsJsonArrays(x, y)
   }
 
   if (
-    a === null || typeof a === 'boolean' || typeof a === 'number' || typeof a === 'string' || Array.isArray(a) ||
-    b === null || typeof b === 'boolean' || typeof b === 'number' || typeof b === 'string' || Array.isArray(b)
+    x === null || typeof x === 'boolean' || typeof x === 'number' || typeof x === 'string' || Array.isArray(x) ||
+    y === null || typeof y === 'boolean' || typeof y === 'number' || typeof y === 'string' || Array.isArray(y)
   ) {
     return false
   }
 
-  return equalsJsonObjects(a, b)
+  return equalsJsonObjects(x, y)
 }
 
 function equalsJsonArrays(xs: JsonArray, ys: JsonArray): boolean {
@@ -42,9 +42,9 @@ function equalsJsonArrays(xs: JsonArray, ys: JsonArray): boolean {
   return true
 }
 
-function equalsJsonObjects(a: JsonObject, b: JsonObject): boolean {
-  const is = Object.keys(a)
-  const js = Object.keys(b)
+function equalsJsonObjects(iXMap: JsonObject, jYMap: JsonObject): boolean {
+  const is = Object.keys(iXMap)
+  const js = Object.keys(jYMap)
 
   // NOTE: 無くてもよいが、パフォーマンスのため。
   if (is.length !== js.length) {
@@ -60,7 +60,11 @@ function equalsJsonObjects(a: JsonObject, b: JsonObject): boolean {
 
   // tslint:disable-next-line:no-loop-statement
   for (const i of is) {
-    if (!equalsJsons(a[i], b[i])) {
+    const j = i
+    const x = iXMap[i]
+    const y = jYMap[j]
+
+    if (!equalsJsons(x, y)) {
       return false
     }
   }
