e086c5a2fb793417ff2e87828309783d1b61b7c5
sueka <mail@sueka.me>
2019-08-04 03:45:58 +0900

    :wrench: Remove formatted messages from span

--- src/components/IntlProvider/index.tsx
+++ src/components/IntlProvider/index.tsx
@@ -9,7 +9,7 @@ type Props =
 // TODO: intl context でない要素を re-render しないようにする。 https://github.com/formatjs/react-intl/issues/234#issuecomment-163366518 によると現時点では難しいらしい。
 // cf. https://github.com/formatjs/react-intl/issues/371#issuecomment-275703796
 const IntlProvider: React.FunctionComponent<Props> = (props) => (
-  <OriginalIntlProvider key={ props.locale } { ...props } />
+  <OriginalIntlProvider key={ props.locale } textComponent={ React.Fragment } { ...props } />
 )
 
 export default IntlProvider
