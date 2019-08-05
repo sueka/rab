e482a8c169bd15135a1a283801c1aa37d0d258d7
sueka <mail@sueka.me>
2019-08-05 22:37:46 +0900

    :bug: Lock the length

--- src/lib/prsg.ts
+++ src/lib/prsg.ts
@@ -1,5 +1,5 @@
 export default function prsg() {
-  const length = Math.floor(Math.log(Number.MAX_SAFE_INTEGER) / Math.log(36)) - 1
+  const length = Math.floor(Math.log(Number.MAX_SAFE_INTEGER) / Math.log(36))
 
-  return Math.floor(36 ** (length + 1) * Math.random()).toString(36).padStart(length, '0')
+  return Math.floor(36 ** length * Math.random()).toString(36).padStart(length, '0')
 }
