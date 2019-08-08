8762de2758c3c7071a2e9340f8a5603a80bd6379
sueka <mail@sueka.me>
2019-08-09 04:38:16 +0900

    :-1: Use fake on production

--- src/container.ts
+++ src/container.ts
@@ -5,16 +5,22 @@ import EnvVarConfigRegistry from './infrastructure/EnvVarConfigRegistry'
 import { Service } from './redux'
 import { CounterService } from './redux/modules/counter'
 import { LocaleSelectorService } from './redux/modules/localeSelector'
+import { ReminderService } from './redux/modules/reminder'
 
 import GetRepo from './useCase/GetRepo'
 import GetRepoImpl from './infrastructure/GetRepoImpl'
+
+import TaskRepository from './domain/repository/TaskRepository'
+import TaskRepositoryFakeImpl from './infrastructure/persistence/double/TaskRepositoryFakeImpl' // TODO:
 
 const container = new Container()
 
 container.bind<ConfigRegistry>('EnvVarConfig').to(EnvVarConfigRegistry)
 container.bind<GetRepo>('GetRepo').to(GetRepoImpl)
+container.bind<TaskRepository>('TaskRepository').to(TaskRepositoryFakeImpl)
 container.bind<Service>('Service').to(Service)
 container.bind<CounterService>(CounterService).toSelf()
 container.bind<LocaleSelectorService>(LocaleSelectorService).toSelf()
+container.bind<ReminderService>(ReminderService).toSelf()
 
 export default container
