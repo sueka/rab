5a85e96f4f8a2bc9c9916e91b5faf20e6b8d0bd1
sueka <mail@sueka.me>
2019-08-13 14:41:34 +0900

    :sparkles: Polyfill Object.fromEntries

---
+++ src/lib/polyfills/Object.fromEntries.ts
@@ -0,0 +1,12 @@
+export default function fromEntries<K extends string, V>(entries: Iterable<[K, V]>): Record<K, V> {
+  const result: Partial<Record<K, V>> = {}
+
+  // tslint:disable-next-line:no-loop-statement
+  for (const [key, value] of entries) {
+
+    // tslint:disable-next-line:no-object-mutation
+    result[key] = value
+  }
+
+  return result as Record<K, V>
+}
