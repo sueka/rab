6696b86aeee978940767ed8f52e429c9f68c4d4e
sueka <mail@sueka.me>
2019-08-24 01:16:11 +0900

    :art: Use escape sequence for control character

--- src/lib/extensions/String/stripMargin.spec.ts
+++ src/lib/extensions/String/stripMargin.spec.ts
@@ -8,18 +8,9 @@ describe('stripMargin', () => {
     expect(stripMargin('||a')).toEqual('|a')
     expect(stripMargin('| |a')).toEqual(' |a')
     expect(stripMargin(' | |a')).toEqual(' |a')
-    expect(stripMargin(`
-      |foo
-      |bar
-      |`)).toEqual('\nfoo\nbar\n')
+    expect(stripMargin('\n      |foo\n      |bar\n      |')).toEqual('\nfoo\nbar\n')
     expect(stripMargin('\n      |foo\r\n      |bar\r      |')).toEqual('\nfoo\r\nbar\r      |')
-    expect(stripMargin('/', `
-      /foo
-      /bar
-      /`)).toEqual('\nfoo\nbar\n')
-    expect(stripMargin('/', `
-      |foo
-      |bar
-      |`)).toEqual('\n      |foo\n      |bar\n      |') // TODO
+    expect(stripMargin('/', '\n      /foo\n      /bar\n      /')).toEqual('\nfoo\nbar\n')
+    expect(stripMargin('/', '\n      |foo\n      |bar\n      |')).toEqual('\n      |foo\n      |bar\n      |')
   })
 })
