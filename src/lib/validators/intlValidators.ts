d5dcfb420baf3da3ab590775fa91fe1f334391f5
sueka <mail@sueka.me>
2019-10-10 00:11:19 +0900

    :bug: Fix bugs w/ asUnionOf

--- src/lib/validators/intlValidators.ts
+++ src/lib/validators/intlValidators.ts
@@ -8,30 +8,30 @@ export const asFormats = asObject<Formats>('a Formats', (input) => ({
 }))
 
 const asIntlNumberFormatOptions = asObject<Intl.NumberFormatOptions>('an Intl.NumberFormatOptions', (input) => ({
-  localeMatcher: optional(asUnionOf(['lookup', 'best fit'] as const))(input.localeMatcher),
-  style: optional(asUnionOf(['decimal', 'currency', 'percent'] as const))(input.style),
+  localeMatcher: optional(asUnionOf('lookup', 'best fit'))(input.localeMatcher),
+  style: optional(asUnionOf('decimal', 'currency', 'percent'))(input.style),
   currency: optional(asString)(input.currency), // ISO 4217 currency code
-  currencyDisplay: optional(asUnionOf(['symbol', 'code', 'name'] as const))(input.currencyDisplay),
+  currencyDisplay: optional(asUnionOf('symbol', 'code', 'name'))(input.currencyDisplay),
   useGrouping: optional(asBoolean)(input.useGrouping),
-  minimumIntegerDigits: optional(asUnionOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21] as const))(input.minimumIntegerDigits),
-  minimumFractionDigits: optional(asUnionOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] as const))(input.minimumFractionDigits),
-  maximumFractionDigits: optional(asUnionOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] as const))(input.maximumFractionDigits),
-  minimumSignificantDigits: optional(asUnionOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21] as const))(input.minimumSignificantDigits),
-  maximumSignificantDigits: optional(asUnionOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21] as const))(input.maximumSignificantDigits),
+  minimumIntegerDigits: optional(asUnionOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21))(input.minimumIntegerDigits),
+  minimumFractionDigits: optional(asUnionOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20))(input.minimumFractionDigits),
+  maximumFractionDigits: optional(asUnionOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20))(input.maximumFractionDigits),
+  minimumSignificantDigits: optional(asUnionOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21))(input.minimumSignificantDigits),
+  maximumSignificantDigits: optional(asUnionOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21))(input.maximumSignificantDigits),
 }))
 
 const asIntlDateTimeFormatOptions = asObject<Intl.DateTimeFormatOptions>('an Intl.DateTimeFormatOptions', (input) => ({
-  localeMatcher: optional(asUnionOf(['lookup', 'best fit'] as const))(input.localeMatcher),
-  weekday: optional(asUnionOf(['narrow', 'short', 'long'] as const))(input.weekday),
-  era: optional(asUnionOf(['narrow', 'short', 'long'] as const))(input.era),
-  year: optional(asUnionOf(['numeric', '2-digit'] as const))(input.year),
-  month: optional(asUnionOf(['numeric', '2-digit', 'narrow', 'short', 'long'] as const))(input.month),
-  day: optional(asUnionOf(['numeric', '2-digit'] as const))(input.day),
-  hour: optional(asUnionOf(['numeric', '2-digit'] as const))(input.hour),
-  minute: optional(asUnionOf(['numeric', '2-digit'] as const))(input.minute),
-  second: optional(asUnionOf(['numeric', '2-digit'] as const))(input.second),
-  timeZoneName: optional(asUnionOf(['short', 'long'] as const))(input.timeZoneName),
-  formatMatcher: optional(asUnionOf(['basic', 'best fit'] as const))(input.formatMatcher),
+  localeMatcher: optional(asUnionOf('lookup', 'best fit'))(input.localeMatcher),
+  weekday: optional(asUnionOf('narrow', 'short', 'long'))(input.weekday),
+  era: optional(asUnionOf('narrow', 'short', 'long'))(input.era),
+  year: optional(asUnionOf('numeric', '2-digit'))(input.year),
+  month: optional(asUnionOf('numeric', '2-digit', 'narrow', 'short', 'long'))(input.month),
+  day: optional(asUnionOf('numeric', '2-digit'))(input.day),
+  hour: optional(asUnionOf('numeric', '2-digit'))(input.hour),
+  minute: optional(asUnionOf('numeric', '2-digit'))(input.minute),
+  second: optional(asUnionOf('numeric', '2-digit'))(input.second),
+  timeZoneName: optional(asUnionOf('short', 'long'))(input.timeZoneName),
+  formatMatcher: optional(asUnionOf('basic', 'best fit'))(input.formatMatcher),
   hour12: optional(asBoolean)(input.hour12),
-  timeZone: optional(unionOf(asUnionOf(['UTC'] as const), asString))(input.localeMatcher), // tz timezone
+  timeZone: optional(unionOf(asUnionOf('UTC'), asString))(input.localeMatcher), // tz timezone
 }))
