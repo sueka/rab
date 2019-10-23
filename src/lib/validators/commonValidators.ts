c970386a84f16f3cee5fe1d6ed75820b902eafd2
sueka <mail@sueka.me>
2019-10-24 00:02:39 +0900

    :sparkles: Enable validators to handle non-JSON input

--- src/lib/validators/commonValidators.ts
+++ src/lib/validators/commonValidators.ts
@@ -16,7 +16,7 @@ import equalsJsons from '~/lib/extensions/Eq/equalsJsons'
  * @param asT {Validator}
  * @throws {never}
  */
-export const failSafe = <A extends Json, T>(asT: (input: A) => T) => (input: A): either.Either<ValidationError, T> => {
+export const failSafe = <A extends unknown, T>(asT: (input: A) => T) => (input: A): either.Either<ValidationError, T> => {
   try {
     return either.right(asT(input))
   } catch (error) {
@@ -28,7 +28,7 @@ export const failSafe = <A extends Json, T>(asT: (input: A) => T) => (input: A):
   }
 }
 
-export const optional = <T>(asT: (input: Json) => T) => (input: Json | undefined): T | undefined => {
+export const optional = <T>(asT: (input: unknown) => T) => (input: unknown | undefined): T | undefined => {
   if (input === undefined) {
     return
   }
@@ -36,7 +36,7 @@ export const optional = <T>(asT: (input: Json) => T) => (input: Json | undefined
   return asT(input)
 }
 
-export const unionOf = <T, U>(asT: (input: Json) => T, asU: (input: Json) => U) => (input: Json): T | U => {
+export const unionOf = <T, U>(asT: (input: unknown) => T, asU: (input: unknown) => U) => (input: unknown): T | U => {
   const t = failSafe(asT)(input)
   const u = failSafe(asU)(input)
 
@@ -55,8 +55,6 @@ export const unionOf = <T, U>(asT: (input: Json) => T, asU: (input: Json) => U) 
   throw new UnreachableError
 }
 
-export const recordOf = <T>(asT: (input: Json) => T) => asObject<Record<string, T>>('a Record', (input) => Object.entries(input).map<[string, T]>(([key, value]) => [key, asT(value)]).reduce<Record<string, T>>((output, [key, value]) => ({ ...output, [key]: value }), {}))
-
 const listOf = <T>(asT: (input: unknown) => T) => (input: unknown): T[] => {
   if (!Array.isArray(input)) {
     throw new ValidationError(typed<[string]>`${ JSON.stringify(input) } is not an array.`)
@@ -65,7 +63,9 @@ const listOf = <T>(asT: (input: unknown) => T) => (input: unknown): T[] => {
   return input.map(asT)
 }
 
-export const asUnionOf = <T extends readonly Json[]>(...options: T) => (input: Json): T[number] => {
+export const recordOf = <T>(asT: (input: unknown) => T) => asObject<Record<string, T>>('a Record', (input) => Object.entries(input).map<[string, T]>(([key, value]) => [key, asT(value)]).reduce<Record<string, T>>((output, [key, value]) => ({ ...output, [key]: value }), {}))
+
+export const asUnionOf = <T extends readonly unknown[]>(...options: T) => (input: unknown): T[number] => {
   if (!options.some((option) => option === input)) {
     throw new ValidationError(typed<[string, string]>`${ JSON.stringify(input) } is neigher ${ conj(', ', ' nor ', options.map(String)) }`)
   }
@@ -84,8 +84,8 @@ export const asUnionOf = <T extends readonly Json[]>(...options: T) => (input: J
  * @param className name of {T} with indefinite article
  * @param asT {ObjectTyper}
  */
-export const asObject = <T>(className: string, asT: (input: JsonObject) => T) => (input: Json): T => {
-  if (input === null || typeof input === 'boolean' || typeof input === 'number' || typeof input === 'string' || Array.isArray(input)) {
+export const asObject = <T>(className: string, asT: (input: any) => T) => (input: unknown): T => { // tslint:disable-line:no-any
+  if (input == null) {
     throw new ValidationError(typed<[string]>`${ JSON.stringify(input) } is not an object.`)
   }
 
@@ -109,23 +109,25 @@ export const asObject = <T>(className: string, asT: (input: JsonObject) => T) =>
   }
 }
 
-export function asConstant<T extends null>(a: T): (input: Json) => T
-export function asConstant<T extends boolean>(a: T): (input: Json) => T
-export function asConstant<T extends number>(a: T): (input: Json) => T
-export function asConstant<T extends string>(a: T): (input: Json) => T
-export function asConstant<T extends JsonArray>(a: T): (input: Json) => T
-export function asConstant<T extends JsonObject>(a: T): (input: Json) => T
-export function asConstant<T extends Json>(a: T): (input: Json) => T {
+export function asConstant<T extends null>(a: T): (input: unknown) => T
+export function asConstant<T extends boolean>(a: T): (input: unknown) => T
+export function asConstant<T extends number>(a: T): (input: unknown) => T
+export function asConstant<T extends string>(a: T): (input: unknown) => T
+export function asConstant<T extends JsonArray>(a: T): (input: unknown) => T
+export function asConstant<T extends JsonObject>(a: T): (input: unknown) => T
+export function asConstant<T extends Json>(a: T): (input: unknown) => T {
   return (input) => {
-    if (!equalsJsons(input, a)) {
-      throw new ValidationError(typed<[string, string]>`${ JSON.stringify(input) } is not ${ JSON.stringify(a) }.`)
+    const inputAsJson = asJson(input)
+
+    if (!equalsJsons(inputAsJson, a)) {
+      throw new ValidationError(typed<[string, string]>`${ JSON.stringify(inputAsJson) } is not ${ JSON.stringify(a) }.`)
     }
 
-    return input as T
+    return inputAsJson as T
   }
 }
 
-export function asBoolean(input: Json): boolean {
+export function asBoolean(input: unknown): boolean {
   if (typeof input !== 'boolean') {
     throw new ValidationError(typed<[string]>`${ JSON.stringify(input) } is not a boolean.`)
   }
@@ -133,7 +135,7 @@ export function asBoolean(input: Json): boolean {
   return input
 }
 
-export function asNumber(input: Json): number {
+export function asNumber(input: unknown): number {
   if (typeof input !== 'number') {
     throw new ValidationError(typed<[string]>`${ JSON.stringify(input) } is not a number.`)
   }
@@ -141,7 +143,7 @@ export function asNumber(input: Json): number {
   return input
 }
 
-export function asString(input: Json): string {
+export function asString(input: unknown): string {
   if (typeof input !== 'string') {
     throw new ValidationError(typed<[string]>`${ JSON.stringify(input) } is not a string.`)
   }
