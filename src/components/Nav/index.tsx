cd5bea38aceacebab596004fe00ccc98e03c677b
sueka <mail@sueka.me>
2019-10-30 22:47:08 +0900

    :sparkles: Make Nav an AppBar

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,23 +1,62 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
+// import classnames from 'classnames'
+// import { createStyles, makeStyles } from '@material-ui/core/styles'
 
-import List from '@material-ui/core/List'
+import AppBar from '@material-ui/core/AppBar'
+import IconButton from '@material-ui/core/IconButton'
+import Menu from '@material-ui/core/Menu'
+import Toolbar from '@material-ui/core/Toolbar'
+import MenuIcon from '@material-ui/icons/Menu'
 
-import ListItemLink from '~/components/ListItemLink'
-import LocaleSelect from '~/containers/LocaleSelect'
+import MenuItemLink from '~/components/MenuItemLink'
+import LocaleSelect from '~/containers/LocaleSelect' // TODO
 
+import classes from './classes.css'
 import messages from './messages'
 
-const Nav: React.FunctionComponent = () => (
-  <>
-    <LocaleSelect />
-    <List component="nav">
-      <ListItemLink to="/"><FormattedMessage { ...messages.home } /></ListItemLink>
-      <ListItemLink to="/counter"><FormattedMessage { ...messages.counter } /></ListItemLink>
-      <ListItemLink to="/info"><FormattedMessage { ...messages.info } /></ListItemLink>
-      <ListItemLink to="/reminder"><FormattedMessage { ...messages.reminder } /></ListItemLink>
-    </List>
-  </>
-)
+const Nav: React.FunctionComponent = () => {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
+
+  const openMenu = React.useCallback<React.MouseEventHandler<HTMLButtonElement>>((event) => {
+    setAnchorEl(event.currentTarget)
+  }, [setAnchorEl])
+
+  const closeMenu = React.useCallback(() => {
+    setAnchorEl(null)
+  }, [setAnchorEl])
+
+  return (
+    <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" onClick={ openMenu }>
+            <MenuIcon />
+          </IconButton>
+          <div className={ classes.Spacer } />
+          <LocaleSelect
+            FormControlProps={ {
+              variant: 'standard',
+            } }
+          />
+        </Toolbar>
+      </AppBar>
+      <Menu open={ anchorEl !== null } anchorEl={ anchorEl } onClose={ closeMenu }>
+        <MenuItemLink to="/" onClick={ closeMenu }>
+          <FormattedMessage { ...messages.home } />
+        </MenuItemLink>
+        <MenuItemLink to="/counter" onClick={ closeMenu }>
+          <FormattedMessage { ...messages.counter } />
+        </MenuItemLink>
+        <MenuItemLink to="/info" onClick={ closeMenu }>
+          <FormattedMessage { ...messages.info } />
+        </MenuItemLink>
+        <MenuItemLink to="/reminder" onClick={ closeMenu }>
+          <FormattedMessage { ...messages.reminder } />
+        </MenuItemLink>
+      </Menu>
+    </>
+  )
+}
 
 export default Nav
