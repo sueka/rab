3222d2c43f0fad2737eac4667f2e2d25c8f578e9
sueka <mail@sueka.me>
2019-10-31 05:00:04 +0900

    :art: Rename shadowed parameters to fix TSLint violations

--- src/components/ListItemLink/index.tsx
+++ src/components/ListItemLink/index.tsx
@@ -5,14 +5,14 @@ import MuiListItem, { ListItemProps as MuiListItemProps } from '@material-ui/cor
 
 type ListItemLinkProps = MuiListItemProps<typeof RouterLink, { button?: true }>
 
-const ListItemLink: React.FunctionComponent<ListItemLinkProps> = React.forwardRef<RouterLink, React.PropsWithoutRef<ListItemLinkProps>>(({ to, button, innerRef, ...props }, ref) => {
+const ListItemLink: React.FunctionComponent<ListItemLinkProps> = React.forwardRef<RouterLink, React.PropsWithoutRef<ListItemLinkProps>>(({ to, button, innerRef, ...listItemProps }, listItem) => {
   // See https://material-ui.com/guides/composition/#link
-  const RouterLinkWithRef = React.forwardRef<RouterLink, RouterLinkProps>((props, ref) => (
-    <RouterLink ref={ ref } innerRef={ innerRef } { ...props } />
+  const RouterLinkWithRef = React.forwardRef<RouterLink, RouterLinkProps>((linkProps, link) => (
+    <RouterLink ref={ link } innerRef={ innerRef } { ...linkProps } />
   ))
 
   return (
-    <MuiListItem button component={ RouterLinkWithRef } to={ to } ref={ ref } { ...props } />
+    <MuiListItem button component={ RouterLinkWithRef } to={ to } ref={ listItem } { ...listItemProps } />
   )
 })
 
--- src/components/MenuItemLink/index.tsx
+++ src/components/MenuItemLink/index.tsx
@@ -5,14 +5,14 @@ import MuiMenuItem, { MenuItemProps as MuiMenuItemProps } from '@material-ui/cor
 
 type MenuItemLinkProps = MuiMenuItemProps<typeof RouterLink, { button?: true }>
 
-const MenuItemLink: React.FunctionComponent<MenuItemLinkProps> = React.forwardRef<RouterLink, React.PropsWithoutRef<MenuItemLinkProps>>(({ to, button, innerRef, ...props }, ref) => {
+const MenuItemLink: React.FunctionComponent<MenuItemLinkProps> = React.forwardRef<RouterLink, React.PropsWithoutRef<MenuItemLinkProps>>(({ to, button, innerRef, ...menuItemProps }, menuItem) => {
   // See https://material-ui.com/guides/composition/#link
-  const RouterLinkWithRef = React.forwardRef<RouterLink, RouterLinkProps>((props, ref) => (
-    <RouterLink ref={ ref } innerRef={ innerRef } { ...props } />
+  const RouterLinkWithRef = React.forwardRef<RouterLink, RouterLinkProps>((linkProps, link) => (
+    <RouterLink ref={ link } innerRef={ innerRef } { ...linkProps } />
   ))
 
   return (
-    <MuiMenuItem button component={ RouterLinkWithRef } to={ to } ref={ ref } { ...props } />
+    <MuiMenuItem button component={ RouterLinkWithRef } to={ to } ref={ menuItem } { ...menuItemProps } />
   )
 })
 
