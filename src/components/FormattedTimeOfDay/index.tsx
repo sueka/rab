ad6652d73140b2553bd516184a66648fc2bc2241
sueka <mail@sueka.me>
2019-11-01 19:02:27 +0900

    :sparkles: Create a component

---
+++ src/components/FormattedTimeOfDay/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import { Omit } from 'react-redux'
+import { FormattedTime } from 'react-intl'
+
+type Props = Omit<FormattedTime.Props, /* 'dateStyle' |  */'weekday' | 'era' | 'year' | 'month' | 'day'>
+
+const FormattedTimeOfDay: React.FunctionComponent<Props> = (props) => (
+  <FormattedTime
+    { ...props }
+    // dateStyle={ undefined }
+    weekday={ undefined }
+    era={ undefined }
+    year={ undefined }
+    month={ undefined }
+    day={ undefined }
+  />
+)
+
+export default FormattedTimeOfDay
