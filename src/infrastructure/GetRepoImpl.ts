e8cfae09fe086b7679ebed6530762e21c8896d62
sueka <mail@sueka.me>
2020-01-27 23:12:23 +0900

    :rotating_light: Revert GetRepoImpl for an unsuccessful response

--- src/infrastructure/GetRepoImpl.ts
+++ src/infrastructure/GetRepoImpl.ts
@@ -1,31 +1,35 @@
-import Octokit from '@octokit/rest'
 import { inject, injectable } from 'inversify'
 
 import ConfigRegistry from '~/config/ConfigRegistry'
+import fetch from '~/lib/fetch'
+import typed from '~/lib/typed'
 import { asGetRepoResponse, asUnsuccessfulResponse } from '~/lib/validators/gitHubApiResourceValidators'
 import GetRepo, { GetRepoInput, GetRepoOutput } from '~/useCase/GetRepo'
 
 @injectable()
 export default class GetRepoImpl implements GetRepo {
-  private octokit: Octokit
-
   constructor(
     @inject('EnvVarConfig') private config: ConfigRegistry
-  ) {
-    this.octokit = new Octokit({
-      baseUrl: this.config.get('GITHUB_API_URL'),
-    })
-  }
+  ) {}
 
   public async apply({ owner, repo }: GetRepoInput): Promise<GetRepoOutput> {
-    const { status, data } = await this.octokit.repos.get({ owner, repo })
+    const gitHubApiUrl = this.config.get('GITHUB_API_URL')
+
+    const { response: { status }, body } = await fetch({
+      method: 'GET',
+      parameterizedEndpoint: typed<[string]>`${ gitHubApiUrl }/repos/:owner/:repo`,
+      params: { owner, repo },
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+      },
+    })
 
     if (status === 200) {
       return {
         successful: true,
         response: {
           status,
-          body: asGetRepoResponse(data),
+          body: asGetRepoResponse(body),
         },
       }
     } else {
@@ -33,7 +37,7 @@ export default class GetRepoImpl implements GetRepo {
         successful: false,
         response: {
           status,
-          body: asUnsuccessfulResponse(data), // FIXME: レスポンス間違ってるかも
+          body: asUnsuccessfulResponse(body),
         },
       }
     }
