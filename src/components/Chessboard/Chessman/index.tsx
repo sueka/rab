a0918dcb95f323a51e9db0cabcd8c5f06e01c9ea
sueka <mail@sueka.me>
2020-01-30 06:53:28 +0900

    :sparkles: Make chessman draggable

--- src/components/Chessboard/Chessman/index.tsx
+++ src/components/Chessboard/Chessman/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import classnames from 'classnames'
+import React, { useMemo } from 'react'
+import { DragObjectWithType, useDrag } from 'react-dnd'
 
 import classes from './classes.css'
 
@@ -6,10 +8,34 @@ export interface Props {
   piece: Chess.Piece
 }
 
-const Chessman: React.FunctionComponent<Props> = ({ piece }) => (
-  <span className={ classes.Chessman }>
-    { piece }
-  </span>
-)
+interface CollectedProps {
+  dragging: boolean
+}
+
+interface DragObject extends DragObjectWithType {
+  piece: Chess.Piece
+}
+
+const Chessman: React.FunctionComponent<Props> = ({ piece }) => {
+  const [{ dragging }, drag] = useDrag<DragObject, unknown, CollectedProps>({
+    item: {
+      type: 'Chessman',
+      piece,
+    },
+    collect: (monitor) => ({
+      dragging: monitor.isDragging(),
+    }),
+  })
+
+  const chessmanClassName = useMemo(() => classnames(classes.Chessman, {
+    [classes.Dragging]: dragging,
+  }), [dragging])
+
+  return (
+    <span ref={ drag } className={ chessmanClassName }>
+      { piece }
+    </span>
+  )
+}
 
 export default Chessman
