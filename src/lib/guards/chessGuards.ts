4d584377e2478334037226344221b28a99b1c3ab
sueka <mail@sueka.me>
2020-02-05 00:40:59 +0900

    :hammer: Make the guard safer

--- src/lib/guards/chessGuards.ts
+++ src/lib/guards/chessGuards.ts
@@ -1,7 +1,10 @@
 import { isObject } from './commonGuards'
 import { isBetween } from './numberGuards'
 
+const isFile = (input: Chess.EasyFile): input is Chess.File => Number.isInteger(input) && isBetween(1, 8)(input)
+const isRank = (input: Chess.EasyRank): input is Chess.Rank => Number.isInteger(input) && isBetween(1, 8)(input)
+
 export const isCoordinates = isObject<Chess.EasyCoordinates, Chess.Coordinates>((input) => ({
-  file: isBetween<Chess.EasyFile, Chess.File>(1, 8)(input.file),
-  rank: isBetween<Chess.EasyRank, Chess.Rank>(1, 8)(input.rank),
+  file: isFile(input.file),
+  rank: isRank(input.rank),
 }))
