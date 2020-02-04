dc2e3c9336c76c5bdda435d9c89ddcb4d675e53b
sueka <mail@sueka.me>
2020-02-05 00:15:19 +0900

    :sparkles: Add guard to test coordinates

---
+++ src/lib/guards/chessGuards.ts
@@ -0,0 +1,7 @@
+import { isObject } from './commonGuards'
+import { isBetween } from './numberGuards'
+
+export const isCoordinates = isObject<'file' | 'rank', number, Chess.EasyCoordinates, Chess.Coordinates>((input) => ({
+  file: isBetween<Chess.EasyFile, Chess.File>(1, 8)(input.file),
+  rank: isBetween<Chess.EasyRank, Chess.Rank>(1, 8)(input.rank),
+}))
