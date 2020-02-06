b3194e6251e9ec68956d3baa01834970046ed8b2
sueka <mail@sueka.me>
2020-02-06 23:37:45 +0900

    :sparkles: Add a VO

---
+++ src/domain/vo/Coordinates.ts
@@ -0,0 +1,13 @@
+import hashCode from '~/lib/extensions/Coordinates/hashCode'
+import ValueObject from './ValueObject'
+
+export default class Coordinates extends ValueObject<Chess.Coordinates> {
+  get hashCode() {
+    // tslint:disable-next-line:no-let
+    let result = 17
+
+    result = 31 * result + hashCode(this.value)
+
+    return result
+  }
+}
