42a3033c8f66a617fea3fa83d08ba022970747b1
sueka <mail@sueka.me>
2020-02-07 02:47:57 +0900

    :memo: :hammer: Add document

--- src/domain/vo/ValueObject.ts
+++ src/domain/vo/ValueObject.ts
@@ -2,6 +2,9 @@ import { ValidationError } from '~/lib/errors'
 import Eq from '~/lib/trait/Eq'
 import Hashable from '~/lib/trait/Hashable'
 
+/**
+ * A primitive ValueObject
+ */
 export default abstract class ValueObject<T> extends Hashable implements Eq {
   constructor(
     public readonly value: T
