7800ae96dcd6f70d7ff4de1781953b7be5360126
sueka <mail@sueka.me>
2020-02-07 00:00:06 +0900

    :memo: Update doc

--- src/lib/trait/Hashable.ts
+++ src/lib/trait/Hashable.ts
@@ -1,7 +1,7 @@
 import Eq from './Eq'
 
 /**
- * Eq であり、さらに a.equals(b) → a.hashCode === b.hashCode な hashCode を持つ。
+ * Eq であり、さらに a.equals(b) → a.hashCode() === b.hashCode() な hashCode を持つ。
  */
 export default abstract class Hashable extends Eq implements Eq {
   public equals(that: this) {
