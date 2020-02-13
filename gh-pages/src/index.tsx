a18123be06d33bcd83b5340d4d16a702ac403149
sueka <mail@sueka.me>
2020-02-13 22:56:22 +0900

    :bug: Omit leading basename

--- gh-pages/src/index.tsx
+++ gh-pages/src/index.tsx
@@ -2,8 +2,14 @@ if (process.env.BASE_URL === undefined || !process.env.BASE_URL.startsWith(windo
   throw new Error // TODO
 }
 
+const basename = process.env.BASE_URL.slice(window.location.origin.length)
+
+if (!window.location.pathname.startsWith(basename)) {
+  throw new Error // TODO
+}
+
 const url = new URL(process.env.BASE_URL)
 
-url.hash = window.location.pathname
+url.hash = window.location.pathname.slice(basename.length)
 
 window.location.replace(url.href)
