21813e1a7f8b144d1fdbb75099546f8f1cb2469d
sueka <mail@sueka.me>
2020-02-25 23:24:03 +0900

    :sparkles: Create a template for pages

---
+++ src/templates/PageTemplate/index.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+
+import Nav from '~/components/Nav'
+import ErrorBoundary from '~/lib/components/ErrorBoundary'
+import typed from '~/lib/typed'
+
+interface PageTemplateProps {
+  children: React.ReactElement<PageProps, React.ComponentType<PageProps>>
+}
+
+type PageProps =
+  & RouteComponentProps
+
+const PageTemplate: React.FunctionComponent<PageTemplateProps> = ({ children }) => {
+  const renderError = useCallback((error: unknown) => {
+    if (error instanceof Error) {
+      return typed<[string]>`${ String(error) }`
+    }
+
+    throw new TypeError(typed<[string]>`${ String(error) } is not an error.`)
+  }, [])
+
+  return (
+    <>
+      <Nav />
+      <ErrorBoundary renderError={ renderError }>
+        { children }
+      </ErrorBoundary>
+    </>
+  )
+}
+
+export const createPage: (Component: React.FunctionComponent<PageProps>) => React.FunctionComponent<PageProps> = (Component) => (props) => (
+  <PageTemplate>
+    <Component { ...props } />
+  </PageTemplate>
+)
+
+export default PageTemplate
