7d76317282d102e87dab28ac36ecedbb6c6429a8
sueka <mail@sueka.me>
2020-02-28 00:09:41 +0900

    :sparkles: Create invariants combinator

---
+++ src/lib/middleware/invariantMiddleware/combineInvariants.ts
@@ -0,0 +1,15 @@
+import { Invariant } from './createInvariantMiddleware'
+
+const combineInvariants: <S extends Record<K, V>, K extends keyof S = keyof S, V extends S[K] = S[K]>(invariants: {
+  [P in keyof S]?: Invariant<S[P]>
+}) => Invariant<S> = (invariants) => (state) => {
+  for (const [moduleName, moduleState] of Object.entries(state)) {
+    if (!invariants[moduleName]?.(moduleState)) {
+      return false
+    }
+  }
+
+  return true
+}
+
+export default combineInvariants
