ace8d4948e93e7a76e7d6fed84663f6e13300980
sueka <mail@sueka.me>
2020-02-28 00:28:55 +0900

    :bug: Skip to check an absent invariant

--- src/lib/middleware/invariantMiddleware/combineInvariants.ts
+++ src/lib/middleware/invariantMiddleware/combineInvariants.ts
@@ -4,7 +4,7 @@ const combineInvariants: <S extends Record<K, V>, K extends keyof S = keyof S, V
   [P in keyof S]?: Invariant<S[P]>
 }) => Invariant<S> = (invariants) => (state) => {
   for (const [moduleName, moduleState] of Object.entries(state)) {
-    if (!invariants[moduleName]?.(moduleState)) {
+    if (!(invariants[moduleName]?.(moduleState) ?? true)) {
       return false
     }
   }
