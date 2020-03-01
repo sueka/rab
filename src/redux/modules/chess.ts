a132d3844b4368034900ae3e463e5f26215b3472
sueka <mail@sueka.me>
2020-03-01 22:58:52 +0900

    :sparkles: Add an action

--- src/redux/modules/chess.ts
+++ src/redux/modules/chess.ts
@@ -61,6 +61,7 @@ const HALF_MOVE = '@@react-app-base/chess/HALF_MOVE' // neither castle nor captu
 const PICK_CHESSMAN = '@@react-app-base/chess/PICK_CHESSMAN'
 const PUT_CHESSMAN = '@@react-app-base/chess/PUT_CHESSMAN'
 const REMOVE_CHESSMAN = '@@react-app-base/chess/REMOVE_CHESSMAN'
+const SET_TARGETS = '@@react-app-base/chess/SET_TARGETS'
 
 const chessActionTypes = [
   RESET_BOARD,
@@ -68,6 +69,7 @@ const chessActionTypes = [
   PICK_CHESSMAN,
   PUT_CHESSMAN,
   REMOVE_CHESSMAN,
+  SET_TARGETS,
 ]
 
 type ResetBoardAction = ReturnType<typeof resetBoard> // TODO: chess 960
@@ -75,6 +77,7 @@ type HalfMoveAction = ReturnType<typeof halfMove>
 type PickChessmanAction = ReturnType<typeof pickChessman>
 type PutChessmanAction = ReturnType<typeof putChessman>
 type RemoveChessmanAction = ReturnType<typeof removeChessman>
+type SetTargets = ReturnType<typeof setTargets>
 
 export type ChessAction =
   | ResetBoardAction
@@ -82,6 +85,7 @@ export type ChessAction =
   | PickChessmanAction
   | PutChessmanAction
   | RemoveChessmanAction
+  | SetTargets
 
 function isChessAction(action: Action): action is ChessAction {
   return chessActionTypes.includes(action.type)
@@ -137,6 +141,13 @@ export const removeChessman = (coord: Chess.Coordinates) => <const> ({
   type: REMOVE_CHESSMAN,
   payload: {
     coord,
+  },
+})
+
+export const setTargets = (targets: Chess.Coordinates[]) => <const> ({
+  type: SET_TARGETS,
+  payload: {
+    targets,
   },
 })
 
@@ -216,6 +227,10 @@ export const createChessReducer: (initialState: ChessState) => Reducer<ChessStat
         ...state.board,
         chessmen: state.board.chessmen.delete(new Coordinates(action.payload.coord)), // TODO
       },
+    }
+    case SET_TARGETS: return {
+      ...state,
+      targets: action.payload.targets,
     }
   }
 }
