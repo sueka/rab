786b7f452e25cccee0112f4ebd51064cb20359b5
sueka <mail@sueka.me>
2020-04-02 21:17:12 +0900

    :alien: :bug: Fix the webpack configuration for Storybook

--- .storybook/webpack.config.ts
+++ .storybook/webpack.config.ts
@@ -2,7 +2,7 @@ import { Configuration } from 'webpack'
 import * as path from 'path'
 
 // NOTE: ! を使い、 baseConfig が所望の構造でなければ実行時 TypeError が発生するようにする。
-module.exports = (baseConfig: Configuration) => {
+module.exports = ({ config: baseConfig }: { config: Configuration }) => {
   baseConfig.resolve!.extensions!.push('.ts', '.tsx')
   baseConfig.resolve!.modules!.push(path.resolve(__dirname, '../src'))
   baseConfig.resolve!.alias!['~'] = path.resolve(__dirname, '../src')
