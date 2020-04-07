873677ce20f151559adabc05820debd592b3d4d1
sueka <mail@sueka.me>
2020-04-08 01:02:59 +0900

    :hammer: Make the webpack config type-safer

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,4 @@
-import { ConfigurationFactory } from 'webpack'
+import { ConfigurationFactory, Configuration } from 'webpack'
 import 'webpack-dev-server'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import DotEnvPlugin from 'dotenv-webpack'
@@ -10,101 +10,107 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const config: ConfigurationFactory = (_env, { mode = 'production' }) => ({
-  mode,
-  bail: true,
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-    modules: [
-      'node_modules',
-      path.resolve(__dirname, 'src'),
-    ],
-    alias: {
-      '~': path.resolve(__dirname, 'src'),
-      ...(mode === 'development' ? {
-        'react-dom': '@hot-loader/react-dom',
-      } : {}),
-    },
-  },
-  entry: path.resolve(__dirname, 'src/index.tsx'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'assets/scripts/[name].[hash:8].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'babel-loader',
-        options: {
-          envName: mode,
-        },
+const createConfig: ConfigurationFactory = (_env, { mode = 'production' }) => {
+  const config: Configuration = {
+    mode,
+    bail: true,
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+      modules: [
+        'node_modules',
+        path.resolve(__dirname, 'src'),
+      ],
+      alias: {
+        '~': path.resolve(__dirname, 'src'),
       },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: {
-                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+    },
+    entry: path.resolve(__dirname, 'src/index.tsx'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'assets/scripts/[name].[hash:8].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'babel-loader',
+          options: {
+            envName: mode,
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                modules: {
+                  localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                },
               },
             },
-          },
-          'postcss-loader',
-        ],
-      }
-    ]
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        sourceMap: true,
-      }),
-    ],
-    splitChunks: {
-      chunks: 'all',
-      maxSize: 1048576,
+            'postcss-loader',
+          ],
+        }
+      ]
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
-      base: `${ process.env.BASE_URL }/`,
-      inject: 'head',
-    }),
-    new DotEnvPlugin({
-      path: path.resolve(__dirname, '.env'),
-      safe: true,
-      systemvars: true,
-    }),
-    new CopyWebpackPlugin(
-      [
-        { from: '.', to: '' },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          sourceMap: true,
+        }),
       ],
-      {
-        context: path.resolve(__dirname, 'public'),
-      }
-    ),
-    ...(mode === 'development' ? [
-      new BundleAnalyzerPlugin({
-        analyzerPort: 10122,
-        openAnalyzer: false,
+      splitChunks: {
+        chunks: 'all',
+        maxSize: 1048576,
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src/index.html'),
+        base: `${ process.env.BASE_URL }/`,
+        inject: 'head',
       }),
-    ] : []),
-  ],
-  ...(mode === 'development' ? {
-    devServer: {
+      new DotEnvPlugin({
+        path: path.resolve(__dirname, '.env'),
+        safe: true,
+        systemvars: true,
+      }),
+      new CopyWebpackPlugin(
+        [
+          { from: '.', to: '' },
+        ],
+        {
+          context: path.resolve(__dirname, 'public'),
+        }
+      ),
+    ],
+    devtool: 'source-map',
+  }
+
+  if (mode === 'development') {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-dom': '@hot-loader/react-dom',
+    }
+
+    config.plugins.push(new BundleAnalyzerPlugin({
+      analyzerPort: 10122,
+      openAnalyzer: false,
+    }))
+
+    config.devServer = {
       contentBase: path.join(__dirname, 'dist'),
       host: '0.0.0.0',
       disableHostCheck: true,
       port: 1234,
       historyApiFallback: true,
-    },
-  } : {}),
-  devtool: 'source-map',
-})
+    }
+  }
 
-export default config
+  return config
+}
+
+export default createConfig
