50237622d0c32cf652b5c77f9bcf94ae5ff8de7c
sueka <mail@sueka.me>
2020-04-08 04:35:22 +0900

    :evergreen_tree: :rotating_light: Correct type-checking for buildStorybook

--- gulpfile.ts
+++ gulpfile.ts
@@ -17,17 +17,17 @@ export const clean: TaskFunction = () => del([...ignored, '!node_modules/**', '!
 const extractMessages = npxTask('extract-messages', ['--flat', '--default-locale=en', '--locales=en,ja', '--output=public/messages', 'src/**/messages.ts'])
 const preTypeCheck = parallel(npxTask('tcm', ['src', '-s']), extractMessages)
 const typeCheck = series(preTypeCheck, npxTask('tsc', ['--noEmit', '-p', '.']))
-const typeCheckForTesting = series(preTypeCheck, npxTask('tsc', ['--noEmit', '-p', './tsconfig.test.json']))
+const typeCheckForDevelopment = series(preTypeCheck, npxTask('tsc', ['--noEmit', '-p', './tsconfig.dev.json']))
 const tslint = npxTask('tslint', ['-p', '.'])
 const stylelint = npxTask('stylelint', ['src/**/*.css'])
 export const lint = parallel(tslint, stylelint)
-const testWithoutCoverage = series(typeCheckForTesting, npxTask('jest'))
-const testWithCoverage = series(typeCheckForTesting, npxTask('jest', ['--coverage']))
+const testWithoutCoverage = series(typeCheckForDevelopment, npxTask('jest'))
+const testWithCoverage = series(typeCheckForDevelopment, npxTask('jest', ['--coverage']))
 export const testInWatchMode = series(preTypeCheck, npxTask('jest', ['--onlyChanged', '--watch', '--watchPathIgnorePatterns', '\'\\.css\\.d\\.ts$\''])) // TODO: interrupt に preTypeCheck を差し込む
-export const updateSnapshot = series(typeCheckForTesting, npxTask('jest', ['--updateSnapshot']))
+export const updateSnapshot = series(typeCheckForDevelopment, npxTask('jest', ['--updateSnapshot']))
 export const test = testWithCoverage
 export const build = parallel(typeCheck, series(() => del(['dist/**/*']), npxTask('webpack')))
-export const buildStorybook = parallel(typeCheck, npxTask('build-storybook'))
+export const buildStorybook = parallel(typeCheckForDevelopment, npxTask('build-storybook'))
 export const buildGhPagesCustom404Page = series(() => del(['gh-pages/dist/**/*']), typeCheck, npxTask('webpack', ['--config', 'gh-pages/webpack.config.ts']))
 export const document = parallel(npxTask('typedoc'))
 
