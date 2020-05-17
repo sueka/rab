82372c3e7cea9ab0445d030e6dbc6e03a741cef7
sueka <mail@sueka.me>
2020-05-18 08:11:29 +0900

    :negative_squared_cross_mark: Add a failing test

--- src/lib/hooks/useOnceForEachEffect.spec.tsx
+++ src/lib/hooks/useOnceForEachEffect.spec.tsx
@@ -27,4 +27,16 @@ describe('useOnceForEachEffect', () => {
     expect(effect).toBeCalledWith('b')
     expect(effect).toBeCalledWith('c')
   })
+
+  it('should call effect with each element of xs the size of identified xs times', () => {
+    const effect = jest.fn()
+
+    const isOdd = (x: number) => Number.isInteger(x) && x % 2 === 1
+
+    renderHook(() => useOnceForEachEffect([1, 2, 3, 4], isOdd, effect))
+
+    expect(effect).toBeCalledTimes(2)
+    expect(effect).toBeCalledWith(1)
+    expect(effect).toBeCalledWith(2)
+  })
 })
