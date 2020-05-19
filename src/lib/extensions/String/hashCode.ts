21ca73058fdc8718674d0a58cea5a44313316c0f
sueka <mail@sueka.me>
2020-05-20 02:45:44 +0900

    :racehorse: Improve the performance

--- src/lib/extensions/String/hashCode.ts
+++ src/lib/extensions/String/hashCode.ts
@@ -3,8 +3,8 @@ export default function hashCode(s: string) {
   let result = 0
 
   // tslint:disable-next-line:no-loop-statement
-  for (const c of s.split('')) {
-    result = 31 * result + c.charCodeAt(0)
+  for (let i = 0; i < s.length; ++i) {
+    result = 31 * result + s.charCodeAt(i)
   }
 
   return result
