c8f7e59365865b8a0940c094dccabea79a176446
sueka <mail@sueka.me>
2020-05-20 03:03:39 +0900

    :memo: :bug: Fix the description

--- src/lib/extensions/String/hashCode.spec.ts
+++ src/lib/extensions/String/hashCode.spec.ts
@@ -1,7 +1,7 @@
 import hashCode from './hashCode'
 
 describe('String#hashCode', () => {
-  it('should work like that of java.lang.Double', () => {
+  it('should work like that of java.lang.String', () => {
     expect(hashCode('')).toBe(0)
     expect(hashCode('a')).toBe(97)
     expect(hashCode('aa')).toBe(3104)
