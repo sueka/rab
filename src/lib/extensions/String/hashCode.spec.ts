c5d8ffb004a56092f8b2d0fb9e4948c449b82d91
sueka <mail@sueka.me>
2020-05-20 02:38:29 +0900

    :negative_squared_cross_mark: Test String#hashCode

---
+++ src/lib/extensions/String/hashCode.spec.ts
@@ -0,0 +1,11 @@
+import hashCode from './hashCode'
+
+describe('Number#hashCode', () => {
+  it('should work like that of java.lang.Double', () => {
+    expect(hashCode('')).toBe(0)
+    expect(hashCode('a')).toBe(97)
+    expect(hashCode('aa')).toBe(3104)
+    expect(hashCode('日本語')).toBe(25921943)
+    expect(hashCode('🌊')).toBe(1773134)
+  })
+})
