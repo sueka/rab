29770cdb2fe34e60eb262b37696b7b32665f188f
sueka <mail@sueka.me>
2020-05-20 02:29:50 +0900

    :white_check_mark: Pass tests

--- src/lib/extensions/Number/hashCode.ts
+++ src/lib/extensions/Number/hashCode.ts
@@ -1,14 +1,14 @@
 export default function hashCode(x: number) {
   const v = doubleToLongBits(x)
 
-  return v ^ (v / 2 ** 32)
+  return Number(v & (1n << 32n) - 1n) ^ Number(v >> 32n)
 }
 
 // TODO: remove
-function doubleToLongBits(value: number): number {
+function doubleToLongBits(value: number): bigint {
   const buf = Buffer.allocUnsafe(8)
 
   buf.writeDoubleBE(value, 0)
 
-  return 2 ** 32 * buf.readUInt32BE(0) + buf.readUInt32BE(4)
+  return (BigInt(buf.readUInt32BE(0)) << 32n) + BigInt(buf.readUInt32BE(4))
 }
