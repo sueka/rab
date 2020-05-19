c3d5fe628bec50a742a236af6a1f81c92f317e02
sueka <mail@sueka.me>
2020-05-20 02:38:49 +0900

    :white_check_mark: Pass an expectation

--- src/lib/extensions/String/hashCode.ts
+++ src/lib/extensions/String/hashCode.ts
@@ -3,7 +3,7 @@ export default function hashCode(s: string) {
   let result = 0
 
   // tslint:disable-next-line:no-loop-statement
-  for (const c of s) {
+  for (const c of s.split('')) {
     result = 31 * result + c.charCodeAt(0)
   }
 
