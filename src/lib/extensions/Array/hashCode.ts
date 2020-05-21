06ee991f52355e2345988e7f32ef63655013ceb5
sueka <mail@sueka.me>
2020-05-22 01:21:04 +0900

    :white_check_mark: Implement Array#hashCode

--- src/lib/extensions/Array/hashCode.ts
+++ src/lib/extensions/Array/hashCode.ts
@@ -1,3 +1,15 @@
-export default function hashCode<T extends boolean | number | string>(_xs: T[]) {
-  throw new Error //
+import '~/lib/extensions/Boolean/Boolean.prototype.hashCode'
+import '~/lib/extensions/Number/Number.prototype.hashCode'
+import '~/lib/extensions/String/String.prototype.hashCode'
+
+export default function hashCode<T extends boolean | number | string>(xs: T[]) {
+  // tslint:disable-next-line:no-let
+  let result = 1n
+
+  // tslint:disable-next-line:no-loop-statement
+  for (const x of xs) {
+    result = result * 31n + BigInt(x.hashCode())
+  }
+
+  return Number(BigInt.asIntN(32, result))
 }
