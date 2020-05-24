2f65a1fc6ae812a9160cbf768b3ebd524fd2b0b9
sueka <mail@sueka.me>
2020-05-24 11:02:15 +0900

    :art: Implement equals w/ canEqual

--- src/lib/trait/HashableEq.ts
+++ src/lib/trait/HashableEq.ts
@@ -9,21 +9,15 @@ export default abstract class HashableEq extends Eq implements Class.Hashable {
   /**
    * NOTE: このメソッドをサブクラスでオーバーライドする場合、 {that} がそのクラスのインスタンスであるかどうかを返すように実装しなければならない。
    */
-  protected canEqual(that: Eq): boolean {
+  protected canEqual(that: Eq): that is HashableEq {
     return that instanceof HashableEq
   }
 
   /**
-   * NOTE: このメソッドをサブクラスでオーバーライドする場合、 {that} がそのクラスのインスタンスでない場合は FALSE を返すように実装しなければならない。
+   * NOTE: このメソッドをサブクラスでオーバーライドする場合、 `this.canEqual(that) && that.canEqual(this) && this.hashCode() === that.hashCode()` でなければ FALSE を返すように実装しなければならない。
    */
   public equals(that: Eq): boolean {
-    if (that instanceof HashableEq) {
-      const thatAsThis = that as this // TODO
-
-      return thatAsThis.canEqual(this) && this.hashCode() === thatAsThis.hashCode()
-    } else {
-      return false
-    }
+    return this.canEqual(that) && that.canEqual(this) && this.hashCode() === that.hashCode()
   }
 
   public abstract hashCode(): number // int
