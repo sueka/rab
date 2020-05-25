6f29d9ab9e6636af6c359828335f5c0409d0d88f
sueka <mail@sueka.me>
2020-05-26 01:58:48 +0900

    :hammer: Declare canEqual in Eq

--- src/lib/trait/Eq.ts
+++ src/lib/trait/Eq.ts
@@ -5,13 +5,32 @@
  *
  *       - a.equals(a) であり、
  *       - a.equals(b) → b.equals(a) であり、かつ
- *       - a.equals(b) ∧ b.equals(c) → a.equals(c) である
+ *       - a.equals(b) ∧ b.equals(c) → a.equals(c) である。
+ *
+ * NOTE: {equals} をオーバーライドする場合、 {canEqual} もオーバーライドしなければならない。
+ *
+ * NOTE: Eq に属する任意の値 a, b について、 a.equals(b) → a.canEqual(b) である。
  *
  * TODO: PartialEq, Congruence 等検討
  */
 export default abstract class Eq implements Class.Eq {
+  /**
+   * {this} が {that} と同値でありうるかどうかを返す。
+   *
+   * NOTE: このメソッドをサブクラスでオーバーライドする場合、 {that} がそのクラスのインスタンスであるかどうかを返すように実装しなければならない。
+   */
+  protected abstract canEqual(that: Eq): boolean
+
+  /**
+   * {this} と {that} が同値かどうかを返す。
+   *
+   * NOTE: このメソッドをオーバーライドする場合、 `this.canEqual(that) && that.canEqual(this)` でなければ FALSE を返すように実装しなければならない。
+   */
   public abstract equals(that: Eq): boolean
 
+  /**
+   * @final
+   */
   public isNotEqualTo(that: Eq): boolean {
     return !this.equals(that)
   }
--- src/lib/trait/HashableEq.ts
+++ src/lib/trait/HashableEq.ts
@@ -1,22 +1,15 @@
 import Eq from './Eq'
 
 /**
- * Eq であり、さらに a.equals(b) → a.hashCode() === b.hashCode() な hashCode と a.equals(b) → a.canEqual(b) ∧ b.canEqual(a) な canEqual を持つ。
- *
- * NOTE: {equals} をオーバーライドする場合、 {canEqual} もオーバーライドしなければならない。
+ * Eq であり、さらに a.equals(b) → a.hashCode() === b.hashCode() な hashCode を持つ。
  */
 export default abstract class HashableEq extends Eq implements Class.Hashable {
-  /**
-   * {this} が {that} と等価でありうるかどうかを返す。
-   *
-   * NOTE: このメソッドをサブクラスでオーバーライドする場合、 {that} がそのクラスのインスタンスであるかどうかを返すように実装しなければならない。
-   */
   protected canEqual(that: Eq): that is HashableEq {
     return that instanceof HashableEq
   }
 
   /**
-   * NOTE: このメソッドをサブクラスでオーバーライドする場合、 `this.canEqual(that) && that.canEqual(this) && this.hashCode() === that.hashCode()` でなければ FALSE を返すように実装しなければならない。
+   * NOTE: このメソッドをオーバーライドする場合、 `this.canEqual(that) && that.canEqual(this) && this.hashCode() === that.hashCode()` でなければ FALSE を返すように実装しなければならない。
    */
   public equals(that: Eq): boolean {
     return this.canEqual(that) && that.canEqual(this) && this.hashCode() === that.hashCode()
