3e068bc680cbc37f97feacdc3015e00ccbea68f2
sueka <mail@sueka.me>
2020-06-11 13:58:44 +0900

    :sparkles: Add an entity

---
+++ src/domain/entity/Reminder.ts
@@ -0,0 +1,47 @@
+import { List } from 'immutable'
+
+import Task from '~/domain/entity/Task'
+import ReminderId from '~/domain/vo/ReminderId'
+import Entity from './Entity'
+
+interface ReminderRequest {
+  id: ReminderId
+  tasks: List<Task>
+}
+
+export default class Reminder extends Entity {
+  private _tasks: List<Task>
+
+  constructor({
+    id,
+    tasks,
+  }: ReminderRequest) {
+    super(id)
+
+    this._tasks = tasks
+  }
+
+  public hashCode() {
+    // tslint:disable-next-line:no-let
+    let result = 17
+
+    result = 31 * result + this.id.hashCode()
+    result = 31 * result + this.tasks.hashCode()
+
+    return result
+  }
+
+  public with({
+    tasks = this.tasks,
+  }: Partial<Alt.Omit<ReminderRequest, 'id'>>): Reminder {
+    return new Reminder({ id: this.id, tasks })
+  }
+
+  get tasks() {
+    return this._tasks
+  }
+
+  set tasks(value) {
+    this._tasks = value // tslint:disable-line:no-object-mutation
+  }
+}
