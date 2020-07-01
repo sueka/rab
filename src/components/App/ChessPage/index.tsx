acc49ed56f2cb89b2982ee6d2779e9af1500ad96
sueka <mail@sueka.me>
2020-07-01 16:23:38 +0900

    :hammer: Provide a context earlier

--- src/components/App/ChessPage/index.tsx
+++ src/components/App/ChessPage/index.tsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import { useSelector, useDispatch } from 'react-redux'
 import { useIntl } from 'react-intl'
+import { compose } from 'redux'
 
 import Chessboard from '~/components/Chessboard'
+import ChessContext from '~/contexts/ChessContext'
+import { State } from '~/redux'
+import { halfMove, pickChessman, releaseChessman } from '~/redux/modules/chess'
 import { createPage } from '~/templates/PageTemplate'
 import messages from './messages'
 
 const ChessPage: React.FunctionComponent = () => {
   const { formatMessage } = useIntl()
 
+  const picking = useSelector((state: State) => state.chess.picking)
+  const targets = useSelector((state: State) => state.chess.targets)
+
+  const dispatch = useDispatch()
+
   return (
     <>
       <Helmet title={ formatMessage(messages.chess) } />
-      <Chessboard />
+      <ChessContext.Provider
+        value={ {
+          picking,
+          targets,
+          halfMove: compose(dispatch, halfMove),
+          pickChessman: compose(dispatch, pickChessman),
+          releaseChessman: compose(dispatch, releaseChessman),
+        } }
+      >
+        <Chessboard />
+      </ChessContext.Provider>
     </>
   )
 }
--- src/components/Chessboard/index.tsx
+++ src/components/Chessboard/index.tsx
@@ -1,28 +1,19 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
-import ChessContext from '~/contexts/ChessContext'
 import Coordinates from '~/domain/vo/Coordinates'
 import { State } from '~/redux'
-import { halfMove, pickChessman, releaseChessman, resetBoard } from '~/redux/modules/chess'
+import { resetBoard } from '~/redux/modules/chess'
 import Chessman from './Chessman'
 import Square from './Square'
 import classes from './classes.css'
 
 interface StateProps {
   board: Chess.Chessboard
-  picking: {
-    chessman: Chess.Chessman
-    source: Chess.Coordinates
-  } | undefined | null
-  targets: Chess.Coordinates[] | undefined | null
 }
 
 interface DispatchProps {
   resetBoard(): void
-  halfMove(chessman: Chess.Chessman, source: Chess.Coordinates, target: Chess.Coordinates): void
-  pickChessman(chessman: Chess.Chessman, source: Chess.Coordinates): void
-  releaseChessman(): void
 }
 
 type Props =
@@ -32,60 +23,45 @@ type Props =
 const files: Chess.File[] = [1, 2, 3, 4, 5, 6, 7, 8]
 const ranks: Chess.Rank[] = [8, 7, 6, 5, 4, 3, 2, 1]
 
-const Chessboard: React.FunctionComponent<Props> = ({ board, picking, targets, resetBoard, halfMove, pickChessman, releaseChessman }) => {
+const Chessboard: React.FunctionComponent<Props> = ({ board, resetBoard }) => {
   useEffect(() => {
     resetBoard() // TODO: skip when LOCATION_CHANGE is dispatched
   }, [resetBoard])
 
   return (
-    <ChessContext.Provider
-      value={ {
-        picking,
-        targets,
-        halfMove,
-        pickChessman,
-        releaseChessman,
-      } }
-    >
-      <table className={ classes.ChessboardTable }>
-        <tbody>
-          { ranks.map((rank) => (
-            <tr key={ rank }>
-              { files.map((file) => {
-                const coord = new Coordinates({ file, rank })
-                const chessman = board.chessmen.get(coord)
+    <table className={ classes.ChessboardTable }>
+      <tbody>
+        { ranks.map((rank) => (
+          <tr key={ rank }>
+            { files.map((file) => {
+              const coord = new Coordinates({ file, rank })
+              const chessman = board.chessmen.get(coord)
 
-                return (
-                  <td key={ file } className={ classes.ChessboardTd }>
-                    <Square coord={ coord }>
-                      { chessman !== undefined ? (
-                        <Chessman chessman={ chessman } coord={ coord } />
-                      ) : null }
-                    </Square>
-                  </td>
-                )
-              }) }
-            </tr>
-          )) }
-        </tbody>
-      </table>
-    </ChessContext.Provider>
+              return (
+                <td key={ file } className={ classes.ChessboardTd }>
+                  <Square coord={ coord }>
+                    { chessman !== undefined ? (
+                      <Chessman chessman={ chessman } coord={ coord } />
+                    ) : null }
+                  </Square>
+                </td>
+              )
+            }) }
+          </tr>
+        )) }
+      </tbody>
+    </table>
   )
 }
 
 // connect
 
-const mapStateToProps = ({ chess: { board, picking, targets } }: State): StateProps => ({
+const mapStateToProps = ({ chess: { board } }: State): StateProps => ({
   board,
-  picking,
-  targets,
 })
 
 const mapDispatchToProps: DispatchProps = {
   resetBoard,
-  halfMove,
-  pickChessman,
-  releaseChessman,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chessboard)
