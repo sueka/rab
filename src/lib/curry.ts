f14b4ce70c8a3131debb82fb70237fc610d2e33a
sueka <mail@sueka.me>
2020-11-10 00:06:05 +0900

    :hammer: Create an interface to type the curry function

--- src/lib/curry.ts
+++ src/lib/curry.ts
@@ -1,3 +1,7 @@
-const curry = <T0, Ts extends unknown[], U>(f: (x: T0, ...xs: Ts) => U) => (x: T0) => (...xs: Ts): U => f(x, ...xs)
+interface CurryFunction {
+  <T0, Ts extends unknown[], U>(f: (x: T0, ...xs: Ts) => U): (x: T0) => (...xs: Ts) => U
+}
+
+const curry: CurryFunction = (f) => (x) => (...xs) => f(x, ...xs)
 
 export default curry
