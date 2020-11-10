2e9d103244116c49a693049d32b509a04fdba4c2
sueka <mail@sueka.me>
2020-11-10 19:18:39 +0900

    :wrench: Cache babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,34 +1,38 @@
-module.exports = {
-  "presets": [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript"
-  ],
-  "plugins": [
-    "@babel/plugin-transform-runtime",
-    "@babel/plugin-syntax-dynamic-import",
-    [
-      "@babel/plugin-proposal-decorators",
-      {
-        "legacy": true
-      }
+module.exports = (api) => {
+  api.cache.using(() => process.env.NODE_ENV === 'development')
+
+  return {
+    "presets": [
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "@babel/preset-typescript"
     ],
-    "babel-plugin-parameter-decorator",
-    [
-      "@babel/plugin-proposal-class-properties",
-      {
-        "loose": true
-      }
+    "plugins": [
+      "@babel/plugin-transform-runtime",
+      "@babel/plugin-syntax-dynamic-import",
+      [
+        "@babel/plugin-proposal-decorators",
+        {
+          "legacy": true
+        }
+      ],
+      "babel-plugin-parameter-decorator",
+      [
+        "@babel/plugin-proposal-class-properties",
+        {
+          "loose": true
+        }
+      ],
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+      "react-intl-auto"
     ],
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-    "react-intl-auto"
-  ],
-  "env": {
-    "production": {
-      "plugins": [
-        "babel-plugin-unassert"
-      ]
+    "env": {
+      "production": {
+        "plugins": [
+          "babel-plugin-unassert"
+        ]
+      }
     }
   }
 }
