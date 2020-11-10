dc295c3198d05599ceefbff03ab6f90f76ed2fc4
sueka <mail@sueka.me>
2020-11-10 19:20:38 +0900

    :art: Format the object that was a JSON

--- babel.config.js
+++ babel.config.js
@@ -2,37 +2,37 @@ module.exports = (api) => {
   api.cache.using(() => process.env.NODE_ENV === 'development')
 
   return {
-    "presets": [
-      "@babel/preset-env",
-      "@babel/preset-react",
-      "@babel/preset-typescript"
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-typescript',
     ],
-    "plugins": [
-      "@babel/plugin-transform-runtime",
-      "@babel/plugin-syntax-dynamic-import",
+    plugins: [
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-syntax-dynamic-import',
       [
-        "@babel/plugin-proposal-decorators",
+        '@babel/plugin-proposal-decorators',
         {
-          "legacy": true
-        }
+          legacy: true,
+        },
       ],
-      "babel-plugin-parameter-decorator",
+      'babel-plugin-parameter-decorator',
       [
-        "@babel/plugin-proposal-class-properties",
+        '@babel/plugin-proposal-class-properties',
         {
-          "loose": true
-        }
+          loose: true,
+        },
       ],
-      "@babel/plugin-proposal-optional-chaining",
-      "@babel/plugin-proposal-nullish-coalescing-operator",
-      "react-intl-auto"
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      'react-intl-auto',
     ],
-    "env": {
-      "production": {
-        "plugins": [
-          "babel-plugin-unassert"
-        ]
-      }
-    }
+    env: {
+      production: {
+        plugins: [
+          'babel-plugin-unassert',
+        ],
+      },
+    },
   }
 }
