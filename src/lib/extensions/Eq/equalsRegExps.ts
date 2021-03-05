fe0ddcf4612d2e2dfbcac32a17bc42c4603e5541
sueka <mail@sueka.me>
2021-03-06 07:47:03 +0900

    :sparkles: Allow you to check if RegExps are equal

---
+++ src/lib/extensions/Eq/equalsRegExps.ts
@@ -0,0 +1,4 @@
+// NOTE: 導入時点では webpack.config.dev.ts から使うためのものなので、 Node.js 動作すれば十分であり、 IE 対応は不要。
+export default function equalsRegExps(x: RegExp, y: RegExp): boolean {
+  return x.source === y.source && x.flags === y.flags
+}
