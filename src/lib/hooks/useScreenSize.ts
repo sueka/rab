426d9c5dbfc2faa6799248e1b3a4959679ef7b64
sueka <mail@sueka.me>
2021-03-11 12:21:03 +0900

    :sparkles: Add a hook to use the screen size

---
+++ src/lib/hooks/useScreenSize.ts
@@ -0,0 +1,19 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export default function useScreenSize() {
+  const [width, setWidth] = useState<number | null>(null)
+  const [height, setHeight] = useState<number | null>(null)
+
+  const updateScreenSize = useCallback(() => {
+    setWidth(globalThis.innerWidth)
+    setHeight(globalThis.innerHeight)
+  }, [])
+
+  useEffect(() => {
+    globalThis.addEventListener('resize', updateScreenSize)
+
+    return () => globalThis.removeEventListener('resize', updateScreenSize)
+  }, [updateScreenSize])
+
+  return { width, height }
+}
