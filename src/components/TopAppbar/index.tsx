c51418bb58bc2e96e425ece77b0af5693a8ed80e
sueka <mail@sueka.me>
2021-03-13 03:16:48 +0900

    :sparkles: Fix the top appbar horizontally
    And also support IE 11

--- src/components/TopAppbar/index.tsx
+++ src/components/TopAppbar/index.tsx
@@ -1,39 +1,78 @@
 import AppBar from '@material-ui/core/AppBar'
 import IconButton from '@material-ui/core/IconButton'
 import Toolbar from '@material-ui/core/Toolbar'
+import { Theme, makeStyles } from '@material-ui/core/styles'
 import MenuIcon from '@material-ui/icons/Menu'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
 import DarkSwitch from '~/components/DarkSwitch'
 import LocaleSelect from '~/components/LocaleSelect' // TODO
+import useScreenSize from '~/lib/hooks/useScreenSize'
+import mergeRefs from '~/lib/mergeRefs'
 import classes from './classes.css'
 
 interface Props {
   onMenuIconButtonClick: React.MouseEventHandler<HTMLButtonElement>
 }
 
-const TopAppbar = React.forwardRef<HTMLDivElement, Props>(({ onMenuIconButtonClick }, ref) => (
-  // TODO: Support IE 11
-  <AppBar position="sticky" ref={ ref }>
-    <Toolbar>
-      <IconButton edge="start" color="inherit" onClick={ onMenuIconButtonClick }>
-        <MenuIcon />
-      </IconButton>
-      <div className={ classes.Spacer } />
-      <DarkSwitch />
-      <LocaleSelect
-        classes={ {
-          label: classes.LocaleSelectLabel,
-          input: classes.LocaleSelectInput,
-          selectIcon: classes.LocaleSelectSelectIcon,
-          inputUnderline: classes.LocaleSelectInputUnderline,
-        } }
-        FormControlProps={ {
-          variant: 'filled',
-        } }
-      />
-    </Toolbar>
-  </AppBar>
-))
+interface StyleProps {
+  topAppbarHeight?: number
+}
+
+const useStyles = makeStyles<Theme, StyleProps, 'Offset'>({
+  Offset: {
+    height: ({ topAppbarHeight }) => topAppbarHeight,
+  },
+})
+
+const TopAppbar = React.forwardRef<HTMLDivElement, Props>(({ onMenuIconButtonClick }, forwardedRef) => {
+  const { width: screenWidth } = useScreenSize()
+  const [height, setHeight] = useState<number | null>(null)
+
+  const ownRef = useCallback<React.RefCallback<HTMLDivElement>>((node) => {
+    // To silence the ESLint rule react-hooks/exhaustive-deps
+    if (screenWidth === null) {
+      return
+    }
+
+    const rect = node?.getBoundingClientRect()
+
+    if (rect === undefined) {
+      return
+    }
+
+    setHeight(rect.height)
+  }, [screenWidth])
+
+  const ref = mergeRefs(forwardedRef, ownRef)
+
+  const jssClasses = useStyles({ topAppbarHeight: height ?? undefined })
+
+  return (
+    <>
+      <AppBar position="fixed" ref={ ref }>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" onClick={ onMenuIconButtonClick }>
+            <MenuIcon />
+          </IconButton>
+          <div className={ classes.Spacer } />
+          <DarkSwitch />
+          <LocaleSelect
+            classes={ {
+              label: classes.LocaleSelectLabel,
+              input: classes.LocaleSelectInput,
+              selectIcon: classes.LocaleSelectSelectIcon,
+              inputUnderline: classes.LocaleSelectInputUnderline,
+            } }
+            FormControlProps={ {
+              variant: 'filled',
+            } }
+          />
+        </Toolbar>
+      </AppBar>
+      <div className={ jssClasses.Offset } />
+    </>
+  )
+})
 
 export default TopAppbar
