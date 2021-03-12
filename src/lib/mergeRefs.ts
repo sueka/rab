b390251f5319a2822d2e3e419777b5d445885c3b
sueka <mail@sueka.me>
2021-03-13 03:01:41 +0900

    :sparkles: Add a helper to merge refs

---
+++ src/lib/mergeRefs.ts
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function mergeRefs<T>(...refs: React.Ref<T>[]): React.RefCallback<T> {
+  return (node) => {
+    for (const ref of refs) { // tslint:disable-line:no-loop-statement
+      if (ref === null) {
+        continue
+      }
+
+      // NOTE: 関数に `current` が設定されたものは React では callback ref と見なされるので、関数かどうかを先に調べる。
+      if (typeof ref === 'function') {
+        ref(node)
+      } else {
+        const mutableRef: React.MutableRefObject<T | null> = ref // FIXME
+
+        mutableRef.current = node // tslint:disable-line:no-object-mutation
+      }
+    }
+  }
+}
