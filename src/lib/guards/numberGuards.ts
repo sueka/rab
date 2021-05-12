35ad50d707aa459356384ace684d3d746394e56d
sueka <mail@sueka.me>
2021-05-13 05:24:49 +0900

    :fire: Delete the unused guards

--- src/lib/guards/numberGuards.ts
+++
@@ -1,3 +0,0 @@
-export const isBetween = <T extends number, U extends T>(lowerBound: number, upperBound: number) => (input: T): input is U => {
-  return lowerBound <= input && input <= upperBound
-}
