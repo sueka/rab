288dbfa65e35db6a6f7bba824e954cae085db62b
sueka <mail@sueka.me>
2021-05-27 12:05:35 +0900

    :sparkles: Add a cookie consent option to the settings page

--- src/components/App/SettingsPage/index.tsx
+++ src/components/App/SettingsPage/index.tsx
@@ -1,44 +1,124 @@
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
+import ListSubheader from '@material-ui/core/ListSubheader'
 import Switch from '@material-ui/core/Switch'
-import React, { useCallback, useContext } from 'react'
+import SecurityIcon from '@material-ui/icons/Security'
+import { useInjection } from 'inversify-react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import Helmet from 'react-helmet'
 import { FormattedMessage, useIntl } from 'react-intl'
-import { useRecoilState } from 'recoil'
+import { useRecoilCallback, useRecoilState, useSetRecoilState } from 'recoil'
 
+import bannerState from '~/atoms/bannerState'
+import cookieConsentObtainedState from '~/atoms/cookieConsentObtainedState'
 import darkState from '~/atoms/darkState'
 import { createPage } from '~/components/PageTemplate'
+import ConfigRegistry from '~/config/ConfigRegistry'
 import DefaultDarkContext from '~/contexts/DefaultDarkContext'
 import { shouldBePresent } from '~/lib/asserters/commonAsserters'
+import Banner from '~/lib/components/Banner'
+import useGtm from '~/lib/hooks/useGtm'
 import messages from './messages'
 
 const SettingsPage: React.FC = () => {
   const { formatMessage } = useIntl()
+  const config = useInjection<ConfigRegistry>('EnvVarConfig')
+  const gtmContainerId = config.get('GTM_CONTAINER_ID')
+  const gtm = useGtm()
+  const setBanner = useSetRecoilState(bannerState)
 
   const [dark, setDark] = useRecoilState(darkState)
+  const [cookieConsentObtained, setCookieConsentObtained] = useRecoilState(cookieConsentObtainedState)
   const { defaultDark } = useContext(DefaultDarkContext)
+  const [cookieConsentChanged, setCookieConsentChanged] = useState(false)
 
   shouldBePresent(defaultDark)
 
-  const handleChange = useCallback((_event, checked) => {
+  const handleDarkThemeChange = useCallback((_event, checked) => {
     setDark(checked)
   }, [])
+
+  const handleAcceptCookiesChange = useCallback((_event, checked) => {
+    setCookieConsentObtained(checked)
+    setCookieConsentChanged(true)
+
+    if (checked) {
+      if (gtmContainerId !== undefined) {
+        gtm.install(gtmContainerId)
+      }
+    }
+  }, [gtm, gtmContainerId])
+
+  const handleReload = useRecoilCallback(({ set }) => () => {
+    location.reload()
+
+    set(bannerState, null) // NOTE: Almost unreachable
+  }, [])
+
+  const handleDontReload = useRecoilCallback(({ set }) => () => {
+    set(bannerState, null)
+  }, [])
+
+  useEffect(() => {
+    if (cookieConsentChanged) {
+      if (!cookieConsentObtained) {
+        setBanner(<Banner
+          leading={ <Avatar>
+            <SecurityIcon />
+          </Avatar> }
+          text={ <FormattedMessage { ...messages.aPageReloadIsRequiredForTheConfigurationChangesToTakeEffectDoYouWantToReloadThePageNow } /> }
+          actions={ <>
+            <Button onClick={ handleReload }>
+              <FormattedMessage { ...messages.reload } />
+            </Button>
+            <Button onClick={ handleDontReload }>
+              <FormattedMessage { ...messages.dontReload } />
+            </Button>
+          </> }
+        />)
+      } else {
+        setBanner(null)
+      }
+    }
+  }, [cookieConsentChanged, cookieConsentObtained])
 
   return (
     <>
       <Helmet title={ formatMessage(messages.settings) } />
-      <Grid item sm={ 6 } md={ 4 } lg={ 3 } xl={ 2 }>
-        <List>
-          <ListItem>
-            <ListItemText primary={ <FormattedMessage { ...messages.darkTheme } /> } />
-            <ListItemSecondaryAction>
-              <Switch checked={ dark ?? defaultDark } onChange={ handleChange } />
-            </ListItemSecondaryAction>
-          </ListItem>
-        </List>
+      <Grid container>
+        <Grid item xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 } xl={ 2 }>
+          <List
+            subheader={ <ListSubheader>
+              <FormattedMessage { ...messages.appearance } />
+            </ListSubheader> }
+          >
+            <ListItem>
+              <ListItemText primary={ <FormattedMessage { ...messages.darkTheme } /> } />
+              <ListItemSecondaryAction>
+                <Switch checked={ dark ?? defaultDark } onChange={ handleDarkThemeChange } />
+              </ListItemSecondaryAction>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 } xl={ 2 }>
+          <List
+            subheader={ <ListSubheader>
+              <FormattedMessage { ...messages.securityAndPrivacy } />
+            </ListSubheader> }
+          >
+            <ListItem>
+              <ListItemText primary={ <FormattedMessage { ...messages.acceptCookies } /> } />
+              <ListItemSecondaryAction>
+                <Switch checked={ cookieConsentObtained } onChange={ handleAcceptCookiesChange } disabled={ gtmContainerId === undefined } />
+              </ListItemSecondaryAction>
+            </ListItem>
+          </List>
+        </Grid>
       </Grid>
     </>
   )
--- src/components/App/SettingsPage/messages.ts
+++ src/components/App/SettingsPage/messages.ts
@@ -2,5 +2,11 @@ import { defineMessages } from 'react-intl'
 
 export default defineMessages({
   settings: 'Settings',
+  appearance: 'Appearance',
   darkTheme: 'Dark theme',
+  securityAndPrivacy: 'Security and privacy',
+  acceptCookies: 'Accept cookies',
+  aPageReloadIsRequiredForTheConfigurationChangesToTakeEffectDoYouWantToReloadThePageNow: 'A page reload is required for the configuration changes to take effect. Do you want to reload the page now?',
+  reload: 'Reload',
+  dontReload: 'Don\'t reload',
 })
