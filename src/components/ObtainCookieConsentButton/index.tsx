0dc4d0514234917608095e6702f5e0362726ff55
sueka <mail@sueka.me>
2021-05-27 10:38:31 +0900

    :sparkles: Install GTM when the cookie consent is obtained

--- src/components/ObtainCookieConsentButton/index.tsx
+++ src/components/ObtainCookieConsentButton/index.tsx
@@ -1,11 +1,14 @@
 import Button from '@material-ui/core/Button'
+import { useInjection } from 'inversify-react'
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { useRecoilCallback } from 'recoil'
 
 import bannerState from '~/atoms/bannerState'
-import cookieConsentObtainedState from '~/atoms/cookieConsentObtainedState'
 import ObtainCookieConsentBanner from '~/components/ObtainCookieConsentBanner'
+import ConfigRegistry from '~/config/ConfigRegistry'
+import { shouldBePresent } from '~/lib/asserters/commonAsserters'
+import useGtm from '~/lib/hooks/useGtm'
 import messages from './messages'
 
 /**
@@ -14,9 +17,17 @@ import messages from './messages'
  * NOTE: https://github.com/mdn/content/blob/f4556b8707dcdd6fec9c6f121a24e6988309f95c/files/en-us/web/api/notification/requestpermission/index.html#L87-L89 と同じような理由で、 Rap では、ユーザーの操作無しにクッキーダイアログを表示することはしない。
  */
 const ObtainCookieConsentButton: React.FC = () => {
+  const config = useInjection<ConfigRegistry>('EnvVarConfig')
+  const gtmContainerId = config.get('GTM_CONTAINER_ID')
+  const gtm = useGtm()
+
   const handleAgree = useRecoilCallback(({ set }) => () => {
+    shouldBePresent(gtmContainerId)
+
     set(bannerState, null)
-  }, [])
+
+    gtm.install(gtmContainerId)
+  }, [gtm, gtmContainerId])
 
   const handleCancel = useRecoilCallback(({ set }) => () => {
     set(bannerState, null)
