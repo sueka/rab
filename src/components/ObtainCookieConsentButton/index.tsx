9a74f761b0bfd349daff371e6f76a51637f84f85
sueka <mail@sueka.me>
2021-05-27 10:14:00 +0900

    :memo: Document

--- src/components/ObtainCookieConsentButton/index.tsx
+++ src/components/ObtainCookieConsentButton/index.tsx
@@ -8,6 +8,11 @@ import cookieConsentObtainedState from '~/atoms/cookieConsentObtainedState'
 import ObtainCookieConsentBanner from '~/components/ObtainCookieConsentBanner'
 import messages from './messages'
 
+/**
+ * クッキーダイアログを表示するボタンを表現する.
+ *
+ * NOTE: https://github.com/mdn/content/blob/f4556b8707dcdd6fec9c6f121a24e6988309f95c/files/en-us/web/api/notification/requestpermission/index.html#L87-L89 と同じような理由で、 Rap では、ユーザーの操作無しにクッキーダイアログを表示することはしない。
+ */
 const ObtainCookieConsentButton: React.FC = () => {
   const [, setBanner] = useRecoilState(bannerState)
   const [, setCookieConsentObtained] = useRecoilState(cookieConsentObtainedState)
