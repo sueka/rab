bf8f7af872c57e3137b18f0b76fc501775a11530
sueka <mail@sueka.me>
2021-06-02 21:38:31 +0900

    :bug: Not show a banner when options other than the use cookies option change

--- src/components/App/SettingsPage/index.tsx
+++ src/components/App/SettingsPage/index.tsx
@@ -9,7 +9,7 @@ import ListSubheader from '@material-ui/core/ListSubheader'
 import Switch from '@material-ui/core/Switch'
 import SecurityIcon from '@material-ui/icons/Security'
 import { useInjection } from 'inversify-react'
-import React, { useCallback, useContext, useEffect, useState } from 'react'
+import React, { useCallback, useContext } from 'react'
 import Helmet from 'react-helmet'
 import { FormattedMessage, useIntl } from 'react-intl'
 import { useRecoilState } from 'recoil'
@@ -36,24 +36,12 @@ const SettingsPage: React.FC = () => {
   const [dark, setDark] = useRecoilState(darkState)
   const [cookieConsentObtained, setCookieConsentObtained] = useRecoilState(cookieConsentObtainedState)
   const { defaultDark } = useContext(DefaultDarkContext)
-  const [cookieConsentChanged, setCookieConsentChanged] = useState(false)
 
   shouldBePresent(defaultDark)
 
   const handleDarkThemeChange = useCallback((_event, checked) => {
     setDark(checked)
   }, [])
-
-  const handleAcceptCookiesChange = useCallback((_event, checked) => {
-    setCookieConsentObtained(checked)
-    setCookieConsentChanged(true)
-
-    if (checked) {
-      if (gtmContainerId !== undefined) {
-        gtm.install(gtmContainerId)
-      }
-    }
-  }, [gtm, gtmContainerId])
 
   const handleReload = useCallback(() => {
     location.reload()
@@ -65,28 +53,32 @@ const SettingsPage: React.FC = () => {
     banner.hide()
   }, [banner])
 
-  useEffect(() => {
-    if (cookieConsentChanged) {
-      if (!cookieConsentObtained) {
-        banner.show(<Banner
-          leading={ <Avatar>
-            <SecurityIcon />
-          </Avatar> }
-          text={ <FormattedMessage { ...messages.aPageReloadIsRequiredForTheConfigurationChangesToTakeEffectDoYouWantToReloadThePageNow } /> }
-          actions={ <>
-            <Button variant="text" color="primary" onClick={ handleReload }>
-              <FormattedMessage { ...messages.reload } />
-            </Button>
-            <Button variant="text" color="primary" onClick={ handleDontReload }>
-              <FormattedMessage { ...messages.dontReload } />
-            </Button>
-          </> }
-        />)
-      } else {
-        banner.hide()
+  const handleAcceptCookiesChange = useCallback((_event, checked) => {
+    setCookieConsentObtained(checked)
+
+    if (checked) {
+      if (gtmContainerId !== undefined) {
+        gtm.install(gtmContainerId)
       }
+
+      banner.hide()
+    } else {
+      banner.show(<Banner
+        leading={ <Avatar>
+          <SecurityIcon />
+        </Avatar> }
+        text={ <FormattedMessage { ...messages.aPageReloadIsRequiredForTheConfigurationChangesToTakeEffectDoYouWantToReloadThePageNow } /> }
+        actions={ <>
+          <Button variant="text" color="primary" onClick={ handleReload }>
+            <FormattedMessage { ...messages.reload } />
+          </Button>
+          <Button variant="text" color="primary" onClick={ handleDontReload }>
+            <FormattedMessage { ...messages.dontReload } />
+          </Button>
+        </> }
+      />)
     }
-  }, [cookieConsentChanged, cookieConsentObtained, banner, handleReload, handleDontReload])
+  }, [gtm, gtmContainerId, handleReload, handleDontReload])
 
   return (
     <>
