ecdd5467fda04028fbc377f905c61b3e31de1f22
sueka <mail@sueka.me>
2023-03-26 16:08:35 +0900

    :sparkles: Create data URL–image decoder

---
+++ src/components/ImageDataUrlDecoder/index.tsx
@@ -0,0 +1,30 @@
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import React, { useCallback, useState } from 'react'
+
+const ImageDataUrlDecoder: React.FC = () => {
+  const [dataUrl, setDataUrl] = useState<string>()
+
+  const handleChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>((event) => {
+    setDataUrl(event.currentTarget.value)
+  }, [])
+
+  return (
+    <Grid container spacing={ 2 }>
+      <Grid item xs={ 12 } sm={ 6 }>
+        <TextField
+          fullWidth
+          multiline
+          maxRows={ 10 }
+          onChange={ handleChange }
+          placeholder="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAAAAAA6fptVAAAADUlEQVQIHQECAP3/AP8BAQEA02HOTAAAAABJRU5ErkJggg=="
+        />
+      </Grid>
+      <Grid item xs={ 12 } sm={ 6 }>
+        <img src={ dataUrl } />
+      </Grid>
+    </Grid>
+  )
+}
+
+export default ImageDataUrlDecoder
