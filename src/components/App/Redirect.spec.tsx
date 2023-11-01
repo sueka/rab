ed7a18d53b832ef514236837cd60dcd70b01d052
sueka <mail@sueka.me>
2023-11-02 07:06:07 +0900

    :white_check_mark: Stop mocking Recoil

--- src/components/App/Redirect.spec.tsx
+++ src/components/App/Redirect.spec.tsx
@@ -1,11 +1,10 @@
 // TODO: index.spec.tsx に統合する?
-// NOTE: useRecoilState をモックせずに RecoilRoot を使うと、 Recoil state の初期化、更新によって re-render が発生し、 <Redirect> が2回以上 render される。これを回避するために useRecoilState をモックし、 Recoil の state が使われないようにしている。別の解決策として、 Redirect のモック実装で useLocation のモック実装を変更する方法がある。
 
 import { render } from '@testing-library/react'
 import { Provider as ServiceProvider } from 'inversify-react'
 import React from 'react'
 import reactRouter, { Redirect } from 'react-router'
-import { useRecoilCallback, useRecoilState } from 'recoil'
+import { RecoilRoot } from 'recoil'
 
 import inversifyContainer from '~/container.dev'
 import App from '.'
@@ -16,29 +15,19 @@ jest.mock('react-router', () => ({
   useLocation: jest.fn(),
 }))
 
-jest.mock('recoil', () => ({
-  ...(jest.requireActual('recoil') as any),
-  useRecoilState: jest.fn(),
-  useRecoilCallback: jest.fn(),
-}))
-
 const mockedReactRouter = reactRouter as jest.Mocked<typeof reactRouter>
 
 describe('App', () => {
   describe('with #fragment', () => {
-    beforeAll(() => {
-      ;(useRecoilState as jest.MockedFunction<typeof useRecoilState>).mockImplementationOnce(() => [[], jest.fn()])
-      ;(useRecoilCallback as jest.MockedFunction<typeof useRecoilCallback>).mockImplementationOnce(() => jest.fn())
-      ;(useRecoilCallback as jest.MockedFunction<typeof useRecoilCallback>).mockImplementationOnce(() => jest.fn())
-    })
-
     it('should redirect to the page whose path-abempty is the fragment when the path-abempty is "/" and the fragment exists', () => {
       mockedReactRouter.useLocation.mockImplementationOnce(() => ({ pathname: '/', hash: '#/foo', search: '', state: undefined }))
 
       const { container } = render(
-        <ServiceProvider container={ inversifyContainer }>
-          <App />
-        </ServiceProvider>
+        <RecoilRoot>
+          <ServiceProvider container={ inversifyContainer }>
+            <App />
+          </ServiceProvider>
+        </RecoilRoot>
       )
 
       expect(container).toMatchSnapshot()
