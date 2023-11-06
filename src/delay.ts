119d5a55cde9097a8cd273cd027a5cd86c62ae1a
sueka <mail@sueka.me>
2023-11-06 15:40:31 +0900

    :sparkles: Add a method timeOut

--- src/delay.ts
+++ src/delay.ts
@@ -1,3 +1,5 @@
-const delay = (ms: number) => new Promise((res) => { setTimeout(res, ms) })
+const delay = (ms: number) => new Promise<void>((resolve) => { setTimeout(() => { resolve() }, ms) })
+const timeOut = (ms: number) => new Promise<never>((_, reject) => { setTimeout(() => { reject('Promise timed out.') }, ms) })
 
 export default delay
+export { timeOut }
