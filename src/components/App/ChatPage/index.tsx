c54a5cefb3f7cd0079439d7ca3d6aeeab04bcd90
sueka <mail@sueka.me>
2023-11-13 15:39:23 +0900

    :sparkles: Allow you to send a message

--- src/components/App/ChatPage/index.tsx
+++ src/components/App/ChatPage/index.tsx
@@ -1,19 +1,27 @@
 import { IconButton, InputAdornment, TextField } from '@material-ui/core'
 import { Send as SendIcon } from '@material-ui/icons'
-import React, { useCallback, useState } from 'react'
+import { useInjection } from 'inversify-react'
+import React, { useCallback, useMemo, useState } from 'react'
 import Helmet from 'react-helmet'
 import { useIntl } from 'react-intl'
 
 import { createPage } from '~/components/PageTemplate'
+import ConfigRegistry from '~/config/ConfigRegistry'
 import messages from './messages'
 
 const ChatPage: React.FC = () => {
   const { formatMessage } = useIntl()
   const [message, setMessage] = useState('')
+  const config = useInjection<ConfigRegistry>('EnvVarConfig')
+  const ws = useMemo(() => new WebSocket(config.get('CHAT_SERVER_URL')), [config])
 
   const handleChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>((event) => {
     setMessage(event.target.value)
   }, [])
+
+  const handleSendButtonClick = useCallback<React.MouseEventHandler>((_event) => {
+    ws.send(message)
+  }, [ws, message])
 
   return (
     <>
@@ -24,7 +32,7 @@ const ChatPage: React.FC = () => {
         InputProps={ {
           endAdornment: (
             <InputAdornment position="end">
-              <IconButton disabled>
+              <IconButton onClick={ handleSendButtonClick }>
                 <SendIcon />
               </IconButton>
             </InputAdornment>
